983f7b519b392c1cbaacef7f48e310550dc8534c
Matt Broadstone <mbroadst@gmail.com>
2018-01-27 18:40:38 -0500

    test(document): added sample test case for Document class

---
+++ Tests/MongoSwiftTests/DocumentTests.swift
@@ -0,0 +1,22 @@
+import Foundation
+import XCTest
+@testable import MongoSwift
+
+final class DocumentTests: XCTestCase {
+    static var allTests : [(String, (DocumentTests) -> () throws -> Void)] {
+        return [
+            ("testDocument", testDocument)
+        ]
+    }
+
+    func testDocument() {
+        var doc = Document();
+        doc["string"] = "test string";
+        doc["true"] = true;
+        doc["false"] = false;
+
+        XCTAssertEqual(doc["string"] as? String, "test string");
+        XCTAssertEqual(doc["true"] as? Bool, true);
+        XCTAssertEqual(doc["false"] as? Bool, false);
+    }
+}
