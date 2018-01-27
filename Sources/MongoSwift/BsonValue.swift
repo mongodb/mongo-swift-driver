688194bd2569d12a4a160b81e714da4423127f39
Matt Broadstone <mbroadst@gmail.com>
2018-01-27 18:33:00 -0500

    feat(document): added skeleton of Document type

---
+++ Sources/MongoSwift/BsonValue.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+public enum BsonType: Int {
+    case invalid = 0x00,
+    double = 0x01,
+    string = 0x02,
+    document = 0x03,
+    array = 0x04,
+    binary = 0x05,
+    undefined = 0x06,
+    objectId = 0x07,
+    boolean = 0x08,
+    dateTime = 0x09,
+    null = 0x0a,
+    regularExpression = 0x0b,
+    dbPointer = 0x0c,
+    javascript = 0x0d,
+    symbol = 0x0e,
+    javascriptWithScope = 0x0f,
+    int32 = 0x10,
+    timestamp = 0x11,
+    int64 = 0x12,
+    decimal128 = 0x13,
+    minKey = 0xff,
+    maxKey = 0x7f
+}
+
+public protocol BsonValue {
+    var bsonType: BsonType { get }
+}
+
+extension String : BsonValue {
+    public var bsonType: BsonType { return .string }
+}
+
+extension Bool : BsonValue {
+    public var bsonType: BsonType { return .boolean }
+}
---
+++ Sources/MongoSwift/Document.swift
@@ -0,0 +1,72 @@
+import libbson
+import Foundation
+
+public class Document {
+    internal var data: UnsafeMutablePointer<bson_t>!
+
+    public init() {
+        data = bson_new()
+    }
+
+    deinit {
+        bson_destroy(data);
+    }
+
+    subscript(key: String) -> BsonValue? {
+        get {
+            var iter: bson_iter_t = bson_iter_t();
+            if (!bson_iter_init(&iter, data)) {
+                return nil;
+            }
+
+            while (bson_iter_next(&iter)) {
+                let ikey = String(cString: bson_iter_key(&iter));
+                if (ikey == key) {
+                    let itype = bson_iter_type(&iter);
+
+                    switch (itype) {
+                    case BSON_TYPE_UTF8:
+                        let len = UnsafeMutablePointer<UInt32>.allocate(capacity: 1)
+                        let value = bson_iter_utf8(&iter, len);
+                        guard let strValue = value else {
+                            // throw some sort of Error?
+                            return nil
+                        }
+
+                        return String(cString: strValue);
+                    case BSON_TYPE_BOOL:
+                        return bson_iter_bool(&iter);
+
+                    default:
+                        return nil
+                    }
+                }
+            }
+
+            return nil
+        }
+
+        set(newValue) {
+            guard let value = newValue else { return }
+
+            switch (value.bsonType) {
+            case .string:
+                let strValue = value as! String
+                let result = bson_append_utf8(data, key, Int32(key.count), strValue, Int32(strValue.count))
+                if (!result) {
+                    print("SOMETHING WENT HORRIBLY WRONG");
+                }
+                break;
+            case .boolean:
+                let boolValue = value as! Bool
+                let result = bson_append_bool(data, key, Int32(key.count), boolValue)
+                if (!result) {
+                    print("SOMETHING WENT HORRIBLY WRONG");
+                }
+                break;
+            default:
+                return
+            }
+        }
+    }
+}
