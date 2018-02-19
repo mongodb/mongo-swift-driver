cd26c8ccc80329e0d9911434f269317094acc903
Matt Broadstone <mbroadst@gmail.com>
2018-02-19 16:33:12 -0500

    fix formatting of the Client implementation

--- Sources/MongoSwift/Client.swift
+++ Sources/MongoSwift/Client.swift
@@ -1,128 +1,120 @@
 import libmongoc
 
 public struct ClientOptions {
-  /// Determines whether the client should retry supported write operations
-  let retryWrites: Bool?
+    /// Determines whether the client should retry supported write operations
+    let retryWrites: Bool?
 }
 
 public struct ListDatabasesOptions: BsonEncodable {
-  /// An optional filter for the returned databases
-  let filter: Document?
+    /// An optional filter for the returned databases
+    let filter: Document?
 
-  /// Optionally indicate whether only names should be returned
-  let nameOnly: Bool?
+    /// Optionally indicate whether only names should be returned
+    let nameOnly: Bool?
 
-  /// An optional session to use for this operation
-  let session: ClientSession?
+    /// An optional session to use for this operation
+    let session: ClientSession?
 
-  /// Convenience constructor for basic construction
-  public init(filter: Document? = nil, nameOnly: Bool? = nil, session: ClientSession? = nil) {
-    self.filter = filter
-    self.nameOnly = nameOnly
-    self.session = session
-  }
-
-  /// Specifies how to encode this type to a `BsonEncoder`
-  public func encode(to encoder: BsonEncoder) throws {
-    if let filter = self.filter {
-      try encoder.encode(filter, forKey: "filter")
+    /// Convenience constructor for basic construction
+    public init(filter: Document? = nil, nameOnly: Bool? = nil, session: ClientSession? = nil) {
+        self.filter = filter
+        self.nameOnly = nameOnly
+        self.session = session
     }
 
-    if let nameOnly = self.nameOnly {
-      try encoder.encode(nameOnly, forKey: "nameOnly")
+    /// Specifies how to encode this type to a `BsonEncoder`
+    public func encode(to encoder: BsonEncoder) throws {
+        try encoder.encode(self.filter, forKey: "filter")
+        try encoder.encode(self.nameOnly, forKey: "nameOnly")
+        try encoder.encode(self.session, forKey: "session")
     }
-
-    if let session = self.session {
-      try encoder.encode(session, forKey: "session")
-    }
-  }
 }
 
 // A MongoDB Client
 public class Client {
-  private var _client = OpaquePointer(bitPattern: 1)
+    private var _client = OpaquePointer(bitPattern: 1)
 
-  /**
-   * Create a new client connection to a MongoDB server
-   *
-   * - Parameters:
-   *   - connectionString: the connection string to connect to
-   *   - options: optional settings
-   */
-  public init(connectionString: String = "mongodb://localhost:27017", options: ClientOptions? = nil) throws {
-    var error = bson_error_t()
-    guard let uri = mongoc_uri_new_with_error(connectionString, &error) else {
-      throw MongoError.invalidUri(message: toErrorString(error))
+    /**
+     * Create a new client connection to a MongoDB server
+     *
+     * - Parameters:
+     *   - connectionString: the connection string to connect to
+     *   - options: optional settings
+     */
+    public init(connectionString: String = "mongodb://localhost:27017", options: ClientOptions? = nil) throws {
+        var error = bson_error_t()
+        guard let uri = mongoc_uri_new_with_error(connectionString, &error) else {
+            throw MongoError.invalidUri(message: toErrorString(error))
+        }
+
+        self._client = mongoc_client_new_from_uri(uri)
+        if self._client == nil {
+            throw MongoError.invalidClient()
+        }
     }
 
-    self._client = mongoc_client_new_from_uri(uri)
-    if self._client == nil {
-      throw MongoError.invalidClient()
-    }
-  }
-
-  /**
-   * Cleanup the internal mongoc_client_t
-   */
-  deinit {
-    close()
-  }
-
-  /**
-   * Creates a client session
-   *
-   * - Parameters:
-   *   - options: The options to use to create the client session
-   *
-   * - Returns: A `ClientSession` instance
-   */
-  func startSession(options: SessionOptions) throws -> ClientSession {
-    return ClientSession()
-  }
-
-  /**
-   * Close the client
-   */
-  func close() {
-    guard let client = self._client else {
-      return
+    /**
+     * Cleanup the internal mongoc_client_t
+     */
+    deinit {
+        close()
     }
 
-    mongoc_client_destroy(client)
-    self._client = nil
-  }
-
-  /**
-   * Get a list of databases
-   *
-   * - Parameters:
-   *   - options: Optional settings
-   *
-   * - Returns: A cursor over documents describing the databases matching provided criteria
-   */
-  func listDatabases(options: ListDatabasesOptions? = nil) throws -> Cursor {
-    let encoder = BsonEncoder()
-    var opts: UnsafeMutablePointer<bson_t>? = nil
-    if let _options = options {
-      opts = (try encoder.encode(_options)).getData()
+    /**
+     * Creates a client session
+     *
+     * - Parameters:
+     *   - options: The options to use to create the client session
+     *
+     * - Returns: A `ClientSession` instance
+     */
+    func startSession(options: SessionOptions) throws -> ClientSession {
+        return ClientSession()
     }
 
-    guard let cursor = mongoc_client_find_databases_with_opts(self._client, opts) else {
-      throw MongoError.invalidResponse()
+    /**
+     * Close the client
+     */
+    func close() {
+        guard let client = self._client else {
+            return
+        }
+
+        mongoc_client_destroy(client)
+        self._client = nil
     }
 
-    return Cursor(fromCursor: cursor)
-  }
+    /**
+     * Get a list of databases
+     *
+     * - Parameters:
+     *   - options: Optional settings
+     *
+     * - Returns: A cursor over documents describing the databases matching provided criteria
+     */
+    func listDatabases(options: ListDatabasesOptions? = nil) throws -> Cursor {
+        let encoder = BsonEncoder()
+        var opts: UnsafeMutablePointer<bson_t>? = nil
+        if let _options = options {
+            opts = (try encoder.encode(_options)).getData()
+        }
 
-  /**
-    * Gets a Database instance for the given database name.
-    *
-    * - Parameters:
-    *   - name: the name of the database to retrieve
-    *
-    * - Returns: a `Database` corresponding to the provided database name
-    */
-  func db(name: String) throws -> Database {
-    return Database()
-  }
+        guard let cursor = mongoc_client_find_databases_with_opts(self._client, opts) else {
+            throw MongoError.invalidResponse()
+        }
+
+        return Cursor(fromCursor: cursor)
+    }
+
+    /**
+     * Gets a Database instance for the given database name.
+     *
+     * - Parameters:
+     *   - name: the name of the database to retrieve
+     *
+     * - Returns: a `Database` corresponding to the provided database name
+     */
+    func db(name: String) throws -> Database {
+        return Database()
+    }
 }
