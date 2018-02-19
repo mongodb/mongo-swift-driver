86a352fa1812e1432b431c7a8e525de1c1aef2d6
Matt Broadstone <mbroadst@gmail.com>
2018-02-19 16:36:16 -0500

    fix formatting of the Cursor implementation

--- Sources/MongoSwift/Cursor.swift
+++ Sources/MongoSwift/Cursor.swift
@@ -2,52 +2,52 @@ import libmongoc
 
 // A Cursor
 public class Cursor: Sequence, IteratorProtocol {
-  private var _cursor = OpaquePointer(bitPattern: 1)
+    private var _cursor = OpaquePointer(bitPattern: 1)
 
-  /// get rid of this
-  init() {}
+    /// get rid of this
+    init() {}
 
-  /**
-    Initializes a new Cursor instance, not meant to be instantiated directly
-   */
-  public init(fromCursor: OpaquePointer) {
-    self._cursor = fromCursor
-  }
-
-  /**
-    Deinitializes a Cursor, cleaning up the internal mongoc_cursor_t
-   */
-  deinit {
-    close()
-  }
-
-  /**
-   * Close the cursor
-   */
-  func close() {
-    guard let cursor = self._cursor else {
-      return
+    /**
+     * Initializes a new Cursor instance, not meant to be instantiated directly
+     */
+    public init(fromCursor: OpaquePointer) {
+        self._cursor = fromCursor
     }
 
-    mongoc_cursor_destroy(cursor)
-    self._cursor = nil
-  }
-
-  /**
-   * Returns the next document in this cursor, or nil
-   */
-  public func next() -> Document? {
-    let out = UnsafeMutablePointer<UnsafePointer<bson_t>?>.allocate(capacity: 1)
-    var error = bson_error_t()
-
-    if !mongoc_cursor_next(self._cursor, out) {
-      if mongoc_cursor_error(self._cursor, &error) {
-        print("cursor error: (domain: \(error.domain), code: \(error.code), message: \(error.message))")
-      }
-
-      return nil
+    /**
+     * Deinitializes a Cursor, cleaning up the internal mongoc_cursor_t
+     */
+    deinit {
+        close()
     }
 
-    return Document(fromData: UnsafeMutablePointer(mutating: out.pointee!))
-  }
+    /**
+     * Close the cursor
+     */
+    func close() {
+        guard let cursor = self._cursor else {
+            return
+        }
+
+        mongoc_cursor_destroy(cursor)
+        self._cursor = nil
+    }
+
+    /**
+     * Returns the next document in this cursor, or nil
+     */
+    public func next() -> Document? {
+        let out = UnsafeMutablePointer<UnsafePointer<bson_t>?>.allocate(capacity: 1)
+        var error = bson_error_t()
+
+        if !mongoc_cursor_next(self._cursor, out) {
+            if mongoc_cursor_error(self._cursor, &error) {
+                print("cursor error: (domain: \(error.domain), code: \(error.code), message: \(error.message))")
+            }
+
+            return nil
+        }
+
+        return Document(fromData: UnsafeMutablePointer(mutating: out.pointee!))
+    }
 }
