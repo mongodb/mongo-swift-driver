be37e9f39585a5edf85ace9be8ae8ab855ab4e5e
Matt Broadstone <mbroadst@gmail.com>
2018-02-20 18:46:50 -0500

    fix formatting

--- Tests/MongoSwiftTests/ClientTests.swift
+++ Tests/MongoSwiftTests/ClientTests.swift
@@ -3,44 +3,44 @@ import Foundation
 import XCTest
 
 final class ClientTests: XCTestCase {
-  static var allTests: [(String, (ClientTests) -> () throws -> Void)] {
-    return [
-      ("testClient", testClient),
-      ("testListDatabases", testListDatabases)
-    ]
-  }
-
-  func testClient() {
-    guard let client = try? Client(connectionString: "mongodb://localhost:27017/") else {
-      XCTAssert(false, "failed to create a client")
-      return
+    static var allTests: [(String, (ClientTests) -> () throws -> Void)] {
+        return [
+            ("testClient", testClient),
+            ("testListDatabases", testListDatabases)
+        ]
     }
 
-    guard let databases = try? client.listDatabases() else {
-      XCTAssert(false, "failed to list databases")
-      return
+    func testClient() {
+        guard let client = try? Client(connectionString: "mongodb://localhost:27017/") else {
+            XCTAssert(false, "failed to create a client")
+            return
+        }
+
+        guard let databases = try? client.listDatabases() else {
+            XCTAssert(false, "failed to list databases")
+            return
+        }
+
+        for database in databases {
+            print(database)
+        }
     }
 
-    for database in databases {
-      print(database)
-    }
-  }
+    func testListDatabases() {
+        guard let client = try? Client(connectionString: "mongodb://localhost:27017/") else {
+            XCTAssert(false, "failed to create a client")
+            return
+        }
 
-  func testListDatabases() {
-    guard let client = try? Client(connectionString: "mongodb://localhost:27017/") else {
-      XCTAssert(false, "failed to create a client")
-      return
-    }
+        guard let databases = try? client.listDatabases(options: ListDatabasesOptions(nameOnly: true)) else {
+            XCTAssert(false, "failed to list databases")
+            return
+        }
 
-    guard let databases = try? client.listDatabases(options: ListDatabasesOptions(nameOnly: true)) else {
-      XCTAssert(false, "failed to list databases")
-      return
+        XCTAssertEqual(Array(databases) as [Document], [
+            ["name": "admin"] as Document,
+            ["name": "config"] as Document,
+            ["name": "local"] as Document
+        ])
     }
-
-    XCTAssertEqual(Array(databases) as [Document], [
-      ["name": "admin"] as Document,
-      ["name": "config"] as Document,
-      ["name": "local"] as Document
-    ])
-  }
 }
