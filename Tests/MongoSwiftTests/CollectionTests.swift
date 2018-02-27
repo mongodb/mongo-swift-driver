5491b324f1b1133ebf28170b4e4bdd9d2974f16f
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2018-02-27 14:15:25 -0500

    use setup/teardown

--- Tests/MongoSwiftTests/CollectionTests.swift
+++ Tests/MongoSwiftTests/CollectionTests.swift
@@ -19,27 +19,51 @@ final class CollectionTests: XCTestCase {
         ]
     }
 
-    let collName = "coll" + String(describing: Date())
+    var coll: MongoSwift.Collection!
+
     let doc1: Document = ["_id": 1, "cat": "dog"]
     let doc2: Document = ["_id": 2, "cat": "cat"]
 
-    func getCollection() throws -> MongoSwift.Collection {
-        return try Client().db("local").collection(collName)
+    /// Set up a single test - run before each testX function
+    override func setUp() {
+        super.setUp()
+        do {
+            coll = try Client().db("collectionTest").collection("coll1")
+        } catch {
+            XCTFail("Setup failed: \(error)")
+        }
+    }
+
+    /// Teardown a single test - run after each testX function
+    override func tearDown() {
+        super.tearDown()
+        do {
+            try coll.drop()
+        } catch {
+            XCTFail("Dropping test collection collectionTest.coll1 failed: \(error)")
+        }
+
+    }
+
+    override class func tearDown() {
+        super.tearDown()
+        do {
+            print("drop DB")
+            // drop database here
+        } catch {
+            XCTFail("Dropping test database collectionTest failed: \(error)")
+        }
     }
 
     func testCount() throws {
-        let coll = try getCollection()
         try coll.insertOne(doc1)
         XCTAssertEqual(try coll.count(), 1)
-
         let options = CountOptions(limit: 5, maxTimeMS: 1000, skip: 5)
         let countWithOptions = try coll.count(options: options)
         XCTAssertEqual(countWithOptions, 0)
-        try coll.drop()
     }
 
     func testInsertOne() throws {
-        let coll = try getCollection()
         guard let result = try coll.insertOne(doc1) else {
             XCTFail("No result from insertion")
             return
@@ -48,64 +72,54 @@ final class CollectionTests: XCTestCase {
 
         try coll.insertOne(doc2)
         XCTAssertEqual(try coll.count(), 2)
-        try coll.drop()
     }
 
     func testAggregate() throws {
-        let coll = try getCollection()
         try coll.insertMany([doc1, doc2])
         let agg = Array(try coll.aggregate([["$project": ["_id": 0, "cat": 1] as Document]]))
         XCTAssertEqual(agg, [["cat": "dog"], ["cat": "cat"]] as [Document])
-        try coll.drop()
     }
 
     func testDrop() throws {
-        let coll = try getCollection()
         try coll.insertMany([doc1, doc2])
         try coll.drop()
         XCTAssertEqual(try coll.count(), 0)
+        // insert something so we don't error when trying to drop
+        // in the cleanup func
+        try coll.insertOne(doc1)
     }
 
     func testInsertMany() throws {
-        let coll = try getCollection()
         try coll.insertMany([doc1, doc2])
         XCTAssertEqual(try coll.count(), 2)
-        try coll.drop()
     }
 
     func testFind() throws {
-        let coll = try getCollection()
         try coll.insertMany([doc1, doc2])
         let findResult = try coll.find(["cat": "cat"])
         XCTAssertEqual(findResult.next(), ["_id": 2, "cat": "cat"])
         XCTAssertNil(findResult.next())
-        try coll.drop()
     }
 
     func testDeleteOne() throws {
-        let coll = try getCollection()
         try coll.insertMany([doc1, doc2])
         guard let deleteOneResult = try coll.deleteOne(["cat": "cat"]) else {
             XCTFail("No result from deleteOne")
             return
         }
         XCTAssertEqual(deleteOneResult.deletedCount, 1)
-        try coll.drop()
     }
 
     func testDeleteMany() throws {
-        let coll = try getCollection()
         try coll.insertMany([doc1, doc2])
         guard let deleteManyResult = try coll.deleteMany([:]) else {
             XCTFail("No result from deleteMany")
             return
         }
         XCTAssertEqual(deleteManyResult.deletedCount, 2)
-        try coll.drop()
     }
 
     func testReplaceOne() throws {
-        let coll = try getCollection()
         try coll.insertOne(doc1)
         guard let replaceOneResult = try coll.replaceOne(
             filter: ["_id": 1], replacement: ["apple": "banana"]) else {
@@ -115,11 +129,9 @@ final class CollectionTests: XCTestCase {
 
         XCTAssertEqual(replaceOneResult.matchedCount, 1)
         XCTAssertEqual(replaceOneResult.modifiedCount, 1)
-        try coll.drop()
     }
 
     func testUpdateOne() throws {
-        let coll = try getCollection()
         try coll.insertMany([doc1, doc2])
         guard let updateOneResult = try coll.updateOne(
             filter: ["_id": 2], update: ["$set": ["apple": "banana"] as Document]) else {
@@ -129,12 +141,9 @@ final class CollectionTests: XCTestCase {
 
         XCTAssertEqual(updateOneResult.matchedCount, 1)
         XCTAssertEqual(updateOneResult.modifiedCount, 1)
-        try coll.drop()
-
     }
 
     func testUpdateMany() throws {
-        let coll = try getCollection()
         try coll.insertMany([doc1, doc2])
         guard let updateManyResult = try coll.updateMany(
             filter: [:], update: ["$set": ["apple": "pear"] as Document]) else {
@@ -144,6 +153,5 @@ final class CollectionTests: XCTestCase {
 
         XCTAssertEqual(updateManyResult.matchedCount, 2)
         XCTAssertEqual(updateManyResult.modifiedCount, 2)
-        try coll.drop()
     }
 }
