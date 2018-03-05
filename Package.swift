4b8de6289b4f6adaee45ce35ac8278a9eab6a897
Matt Broadstone <mbroadst@gmail.com>
2018-03-05 14:55:29 -0500

    SWIFT-47 remove vendored libmongoc, link to system libraries
     * remove vendored versions of libmongoc/libbson
    
     * update spm Package.swift to depend on swift-mongoc/swift-bson

--- Package.swift
+++ Package.swift
@@ -1,12 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
-
 let package = Package(
     name: "MongoSwift",
-    targets: [
-        Target(name: "libmongoc",
-               dependencies: ["libbson"]),
-        Target(name: "MongoSwift",
-               dependencies: ["libmongoc"])
+    products: [
+        .library(name: "MongoSwift", targets: ["MongoSwift"])
     ],
-    exclude: ["Tests/Specs"]
+    dependencies: [
+        .package(url: "ssh://git@github.com/10gen/swift-bson", .branch("master")),
+        .package(url: "ssh://git@github.com/10gen/swift-mongoc", .branch("master"))
+    ],
+    targets: [
+        .target(name: "MongoSwift", dependencies: ["libmongoc"]),
+        .testTarget(name: "MongoSwiftTests", dependencies: ["MongoSwift"])
+    ]
 )
+
