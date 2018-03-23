c090e193ad3f688a7ca629c70f92827fe07c93b6
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2018-03-23 00:39:34 -0400

    comment and use filter

--- Tests/MongoSwiftBenchmarks/benchmark.py
+++ Tests/MongoSwiftBenchmarks/benchmark.py
@@ -1,10 +1,13 @@
-import sys
 import re
+import sys
 
-results = [line for line in sys.stdin.read().split('\n') if "measured" in line]
+# this should be run from the root project directory with:
+# `make benchmark 2>&1 | python Tests/MongoSwiftBenchmarks/benchmark.py`
+# the test results go to stderr so the 2>&1 is to redirect stderr to stdout.
+output = sys.stdin.read()
+results = filter(lambda x: "measured" in x, output.split('\n'))
 
 benchmarks = {}
-
 for r in results:
 	name = re.search(r"\[.*?\]", r).group(0)[22:-1]
 	avg = re.search(r"average: .*?,", r).group(0)[9:-1]
