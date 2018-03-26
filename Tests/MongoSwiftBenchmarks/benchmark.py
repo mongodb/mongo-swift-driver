ba4b8dde014ee44d724369a00ca78d05b9b60163
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2018-03-26 16:21:12 -0400

    cleanup and store values

--- Tests/MongoSwiftBenchmarks/benchmark.py
+++ Tests/MongoSwiftBenchmarks/benchmark.py
@@ -2,12 +2,9 @@ import re
 import sys
 
 # this script should be run from the root project directory with:
-# `make benchmark 2>&1 | python Tests/MongoSwiftBenchmarks/benchmark.py`
-# the test results go to stderr so the 2>&1 is to redirect stderr to stdout.
-# output = sys.stdin.read()
-# results = filter(lambda x: "Median time" in x or "Score" in x, output.split('\n'))
-
-results = ["Results for for -[MultiDocumentBenchmarks testFindManyAndEmptyCursor]: median time 0.022 seconds, score 705.7451 MB/s"]
+# `make benchmark | python Tests/MongoSwiftBenchmarks/benchmark.py`
+output = sys.stdin.read()
+results = filter(lambda x: "median time" in x, output.split('\n'))
 
 benchmarks = {}
 for r in results:
@@ -15,11 +12,7 @@ for r in results:
 	name = match.group("name")
 	time = match.group("seconds")
 	score = match.group("score")
-	print name
-	print time
-	print score
-	#avg = re.search(r"average: .*?,", r).group(0)[9:-1]
-	#benchmarks[name] = float(avg)
+	benchmarks[name] = {"time": float(time), "score": float(score)}
 
 print benchmarks
 
