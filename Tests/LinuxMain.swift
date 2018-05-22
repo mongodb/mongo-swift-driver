fa75d9ba755b200ab7decc633561269682804c12
Matt Broadstone <mbroadst@gmail.com>
2018-05-22 18:43:47 -0400

    Add LinuxMain for testing the driver on linux

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,14 +2,6 @@ import XCTest
 import MongoSwiftTests
 
 var tests = [XCTestCaseEntry]()
-tests += ClientTests.allTests()
-tests += CodecTests.allTests()
-tests += CollectionTests.allTests()
-tests += CommandMonitoringTests.allTests()
-tests += CrudTests.allTests()
-tests += DatabaseTests.allTests()
-tests += DocumentTests.allTests()
-tests += ReadWriteConcernTests.allTests()
-tests += SDAMMonitoringTests.allTests()
+tests += MongoSwiftTests.allTests()
 
 XCTMain(tests)
