3a76e958db4f5738e5f22f72ea2af47f499ccc62
may <mehrabhoque@gmail.com>
2018-10-25 14:00:09 -0400

    Fix linter issues in Kitura example

--- Examples/Kitura/Sources/KituraExample/main.swift
+++ Examples/Kitura/Sources/KituraExample/main.swift
@@ -12,7 +12,7 @@ let collection = try client.db("home").collection("kittens", withType: Kitten.se
 let router: Router = {
   let router = Router()
 
-  router.get("kittens") { request, response, next in
+  router.get("kittens") { _, response, _ in
     let docs = try collection.find()
     response.send(Array(docs))
   }
