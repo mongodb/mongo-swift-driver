72058032dddf45bd104e9ecc9e8365e50637af15
may <mehrabhoque@gmail.com>
2018-10-29 15:00:20 -0400

    Fix linter issue from Github suggestion

--- Sources/MongoSwift/BSON/BSONValue.swift
+++ Sources/MongoSwift/BSON/BSONValue.swift
@@ -788,8 +788,8 @@ public struct Timestamp: BSONValue, Equatable, Codable {
  *  e.g.
  *      4.0 (Double) != 4 (Int)
  *
- *  NOTE: This function will always return `false` if it is used with two arrays that are not of the type `[BSONValue]` or
- *  `[BSONValue?]`, because any arrays that are not of these types are not valid `BSONValue`'s.
+ *  NOTE: This function will always return `false` if it is used with two arrays that are not of the type `[BSONValue]`
+ *  or `[BSONValue?]`, because any arrays that are not of these types are not valid `BSONValue`'s.
  *
  *  * - Parameters:
  *   - lhs: The left-hand-side `BSONValue` to compare.
