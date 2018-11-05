0b169a18d24222ef7099fa19c9431be4f2a8e1af
may <mehrabhoque@gmail.com>
2018-11-05 15:56:29 -0500

    Move non-BSONValue array check outside of loop
    Having it inside gets us no value and is actually doing extra work that
    isn't needed.

--- Sources/MongoSwift/BSON/BSONValue.swift
+++ Sources/MongoSwift/BSON/BSONValue.swift
@@ -102,11 +102,11 @@ extension Array: BSONValue {
 
     public func encode(to storage: DocumentStorage, forKey key: String) throws {
         // An array is just a document with keys '0', '1', etc. corresponding to indexes
+        if self as? [BSONValue?] == nil {
+            throw MongoError.invalidArgument(message: "Cannot encode a non-BSONValue array element")
+        }
         var arr = Document()
         for (i, v) in self.enumerated() {
-            if self as? [BSONValue?] == nil {
-                throw MongoError.invalidArgument(message: "Cannot encode a non-BSONValue array element: \(v)")
-            }
             try arr.setValue(forKey: String(i), to: v as? BSONValue)
         }
         guard bson_append_array(storage.pointer, key, Int32(key.count), arr.data) else {
