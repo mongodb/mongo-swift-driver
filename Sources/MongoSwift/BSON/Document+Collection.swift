8239b1768b2a76ab0d14c91b94b22b32c94edfa2
may <mehrabhoque@gmail.com>
2018-11-09 13:59:46 -0500

    Make start and end index simpler, move subscript()

--- Sources/MongoSwift/BSON/Document+Collection.swift
+++ Sources/MongoSwift/BSON/Document+Collection.swift
@@ -8,23 +8,23 @@
 import Foundation
 
 extension Document: Collection {
-    public subscript(position: Int) -> Document.KeyValuePair {
-        return (self.keys[position], self.values[position])
-    }
-
     public var startIndex: Int {
         precondition(self.count > 0)
-        return self.keys.startIndex
+        return 0
     }
 
     public var endIndex: Int {
         precondition(self.count > 0)
-        return self.keys.endIndex
+        return self.count
     }
 
     public func index(after i: Int) -> Int {
         // Index must be a valid one, meaning it must exist somewhere in self.keys.
         precondition(self.startIndex ... self.endIndex - 1 ~= i)
         return self.keys.index(after: i)
+    }
+
+    public subscript(position: Int) -> Document.KeyValuePair {
+        return (self.keys[position], self.values[position])
     }
 }
