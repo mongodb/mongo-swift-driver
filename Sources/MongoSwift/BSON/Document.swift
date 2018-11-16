df58d709819bde1b53cb4c38f11a77c374d75a5e
may <mehrabhoque@gmail.com>
2018-11-16 13:13:18 -0500

    Remove no-longer relevant comment about nil & NSNull

--- Sources/MongoSwift/BSON/Document.swift
+++ Sources/MongoSwift/BSON/Document.swift
@@ -270,9 +270,6 @@ public struct Document: ExpressibleByDictionaryLiteral, ExpressibleByArrayLitera
             throw MongoError.bsonDecodeError(message: "BSON buffer is unexpectedly too small (< 5 bytes)")
         }
 
-        // TODO: Because documents can hold null as a valid value for a key, this currently means that there is no way
-        // to be sure that the return value from this function suggests that the key does not exist or if the key's
-        // value is nil. This should be considered for SWIFT-193.
         guard iter.move(to: key) else {
             return nil
         }
