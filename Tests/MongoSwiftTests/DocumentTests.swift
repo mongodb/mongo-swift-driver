d90db4bb68d21ae0ed1f12b4e03fb643bfa0fbf8
may <mehrabhoque@gmail.com>
2018-11-20 17:14:46 -0500

    Add tests for all new Collection methods & indices

--- Tests/MongoSwiftTests/DocumentTests.swift
+++ Tests/MongoSwiftTests/DocumentTests.swift
@@ -586,31 +586,98 @@ final class DocumentTests: MongoSwiftTestCase {
         expect(noops).to(equal(["null": 5, "maxkey": "hi", "minkey": false]))
     }
 
-    func testDocumentConformanceToCollection() throws {
-        func trueDocCount(_ document: Document) -> Int {
-            return Int(bson_count_keys(document.data))
+    private struct CollectionConformance {
+        private static func checkCount(_ document: Document) -> Bool {
+            return document.countFast == Int(bson_count_keys(document.data))
         }
-        let secondKey = "b"
-        let secondVal: BSONValue = 2
 
-        var doc: Document = [ "a": 4, secondKey: secondVal ]
-        expect(doc.startIndex).to(equal(0))
-        expect(doc.endIndex).to(equal(doc.count))
-        expect(doc.index(after: doc.index(after: doc.startIndex))).to(equal(doc.endIndex))
-        expect(doc[1].key).to(equal(secondKey))
-        expect(doc[1].value).to(bsonEqual(secondVal))
+        fileprivate static func testDocumentIndexLogic() {
+            let doc: Document = ["a": 3, "b": 4]
 
-        // TODO: Change these `count` to `countFast` eventually
-        expect(doc.count).to(equal(trueDocCount(doc)))
-        let otherDoc: Document = ["c": 3, "d": "hello world"]
-        try doc.merge(otherDoc)
-        expect(doc.count).to(equal(trueDocCount(doc)))
+            // doc.startIndex, doc.endIndex, doc.index(after:), etc.
+            expect(doc.startIndex).to(equal(0))
+            expect(doc.endIndex).to(equal(doc.count))
+            expect(doc.index(after: doc.index(after: doc.startIndex))).to(equal(doc.endIndex))
+            expect(doc[1].key).to(equal("b"))
+            expect(doc[1].value).to(bsonEqual(4))
 
-        doc["new_key"] = "new value"
-        expect(doc.count).to(equal(trueDocCount(doc)))
+            // doc.indices
+            expect(doc.indices.count).to(equal(Int(bson_count_keys(doc.data))))
+            expect(doc.indices.startIndex).to(equal(doc.startIndex))
+            expect(doc.indices[1]).to(equal(doc.index(after: doc.startIndex)))
+            expect(doc.indices.endIndex).to(equal(doc.endIndex))
 
-        debugPrint("Doc: \(doc) with countfast: \(doc.countFast)")
-        let removedElem = doc.removeFirst()
-        debugPrint("removed elem: \(removedElem) and doc: \(doc) and now doc has countFast: \(doc.countFast)")
+            // doc.first
+            let firstElem = doc[doc.startIndex]
+            expect(doc.first?.key).to(equal(firstElem.key))
+            expect(doc.first?.value).to(bsonEqual(firstElem.value))
+
+            // doc.distance
+            expect(doc.distance(from: doc.startIndex, to: doc.endIndex)).to(equal(doc.countFast))
+            expect(doc.distance(from: doc.index(after: doc.startIndex), to: doc.endIndex)).to(equal(doc.countFast - 1))
+
+            // doc.formIndex
+            var firstIndex = 0
+            doc.formIndex(after: &firstIndex)
+            expect(firstIndex).to(equal(doc.index(after: doc.startIndex)))
+
+            // doc.index(offsetBy:), doc.index(offsetBy:,limitedBy:)
+            expect(doc.index(doc.startIndex, offsetBy: 2)).to(equal(doc.endIndex))
+            expect(doc.index(doc.startIndex, offsetBy: 2, limitedBy: doc.endIndex)).to(equal(doc.endIndex))
+            expect(doc.index(doc.startIndex, offsetBy: 99, limitedBy: 1)).to(beNil())
+
+            // firstIndex(where:)
+            // TODO: Perhaps make Document.KeyValuePair conform to Equatable?
+            expect(doc.firstIndex() { return $0.key == "a" && bsonEquals($0.value, 3) }).to(equal(doc.startIndex))
+        }
+
+        fileprivate static func testMutators() {
+            var doc: Document = ["a": 3, "b": 2, "c": 5, "d": 4]
+
+            // doc.removeFirst
+            let firstElem = doc.removeFirst()
+            expect(firstElem.key).to(equal("a"))
+            expect(firstElem.value).to(bsonEqual(3))
+            expect(doc).to(equal(["b": 2, "c": 5, "d": 4]))
+            expect(CollectionConformance.checkCount(doc)).to(beTrue())
+
+            // doc.removeFirst(k:)
+            doc.removeFirst(2)
+            expect(doc).to(equal(["d": 4]))
+            expect(CollectionConformance.checkCount(doc)).to(beTrue())
+
+            // doc.popFirst
+            let lastElem = doc.popFirst()
+            expect(lastElem?.key).to(equal("d"))
+            expect(lastElem?.value).to(bsonEqual(4))
+            expect(doc).to(equal([:]))
+            expect(CollectionConformance.checkCount(doc)).to(beTrue())
+        }
+
+        fileprivate static func testPrefixSuffix() {
+            let doc: Document = ["a": 3, "b": 2, "c": 5, "d": 4, "e": 3]
+
+            let upToPrefixDoc = doc.prefix(upTo: 2)
+            let throughPrefixDoc = doc.prefix(through: 1)
+            let suffixDoc = doc.suffix(from: 1)
+
+            // doc.prefix(upTo:)
+            expect(upToPrefixDoc).to(equal(["a": 3, "b": 2]))
+            expect(CollectionConformance.checkCount(upToPrefixDoc)).to(beTrue())
+
+            // doc.prefix(through:)
+            expect(throughPrefixDoc).to(equal(["a": 3, "b": 2]))
+            expect(CollectionConformance.checkCount(throughPrefixDoc)).to(beTrue())
+
+            // doc.suffix
+            expect(suffixDoc).to(equal(["b": 2, "c": 5, "d": 4, "e": 3]))
+            expect(CollectionConformance.checkCount(suffixDoc)).to(beTrue())
+        }
+    }
+
+    func testDocumentConformanceToCollection() throws {
+        CollectionConformance.testDocumentIndexLogic()
+        CollectionConformance.testMutators()
+        CollectionConformance.testPrefixSuffix()
     }
 }
