94a8cfc93e793b129443d39af9bfd88c0a6d3ff6
may <mehrabhoque@gmail.com>
2018-11-21 11:58:00 -0500

    Add doc comment for Collection extension

--- Sources/MongoSwift/BSON/Document+Collection.swift
+++ Sources/MongoSwift/BSON/Document+Collection.swift
@@ -1,12 +1,8 @@
-//
-//  Document+Collection.swift
-//  MongoSwift
-//
-//  Created by may on 11/8/18.
-//
-
 import Foundation
 
+/// An extension of `Document` to make it conform to the `Collection` protocol.
+/// This gives guarantees on non-destructive iteration, and offers an indexed
+/// ordering to the key value pairs in the document.
 extension Document: Collection {
     /// Returns the start index of the Document.
     public var startIndex: Int {
