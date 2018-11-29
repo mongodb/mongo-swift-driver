9f58013e9858249ebefcc2b5f685a91b18c81422
kmahar <kaitlinmahar@gmail.com>
2018-11-29 16:02:12 -0500

    newline and comment

--- Sources/MongoSwift/Utils.swift
+++ Sources/MongoSwift/Utils.swift
@@ -1,8 +1,8 @@
 /// An extension to add a custom method for deallocation.
 internal extension UnsafeMutablePointer {
-	/// `deallocate(capacity:)` is deprecated as of Swift 4.1, but
-	/// its preferred alternative `deallocate()` does not exist
-	/// in < 4.1.
+	/// `deallocate(capacity:)` is deprecated as of Swift 4.1, but its replacement
+	/// `deallocate()` doesn't exist in < 4.1. this method calls the correct variant
+	/// based on swift version.
 	internal func versionCheckDeallocate() {
 		#if swift(>=4.1)
 		self.deallocate()
