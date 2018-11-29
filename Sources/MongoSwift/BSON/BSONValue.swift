7be7930e1dc397c3e11cdf3677e7691531281e56
kmahar <kaitlinmahar@gmail.com>
2018-11-29 16:19:19 -0500

    refactor

--- Sources/MongoSwift/BSON/BSONValue.swift
+++ Sources/MongoSwift/BSON/BSONValue.swift
@@ -81,7 +81,7 @@ extension Array: BSONValue {
         let array = UnsafeMutablePointer<UnsafePointer<UInt8>?>.allocate(capacity: 1)
         defer {
             array.deinitialize(count: 1)
-            array.versionCheckDeallocate()
+            array.deallocate()
         }
         bson_iter_array(&iter.iter, &length, array)
 
@@ -194,7 +194,7 @@ public struct Binary: BSONValue, Equatable, Codable {
         let dataPointer = UnsafeMutablePointer<UnsafePointer<UInt8>?>.allocate(capacity: 1)
         defer {
             dataPointer.deinitialize(count: 1)
-            dataPointer.versionCheckDeallocate()
+            dataPointer.deallocate()
         }
         bson_iter_binary(&iter.iter, &subtype, &length, dataPointer)
 
@@ -274,13 +274,13 @@ internal struct DBPointer: BSONValue {
         let collectionPP = UnsafeMutablePointer<UnsafePointer<Int8>?>.allocate(capacity: 1)
         defer {
             collectionPP.deinitialize(count: 1)
-            collectionPP.versionCheckDeallocate()
+            collectionPP.deallocate()
         }
 
         let oidPP = UnsafeMutablePointer<UnsafePointer<bson_oid_t>?>.allocate(capacity: 1)
         defer {
             oidPP.deinitialize(count: 1)
-            oidPP.versionCheckDeallocate()
+            oidPP.deallocate()
         }
 
         bson_iter_dbpointer(&iter.iter, &length, collectionPP, oidPP)
@@ -480,7 +480,7 @@ public struct CodeWithScope: BSONValue, Equatable, Codable {
         let scopePointer = UnsafeMutablePointer<UnsafePointer<UInt8>?>.allocate(capacity: 1)
         defer {
             scopePointer.deinitialize(count: 1)
-            scopePointer.versionCheckDeallocate()
+            scopePointer.deallocate()
         }
 
         let code = String(cString: bson_iter_codewscope(&iter.iter, &length, &scopeLength, scopePointer))
@@ -684,7 +684,7 @@ public struct RegularExpression: BSONValue, Equatable, Codable {
         let options = UnsafeMutablePointer<UnsafePointer<Int8>?>.allocate(capacity: 1)
         defer {
             options.deinitialize(count: 1)
-            options.versionCheckDeallocate()
+            options.deallocate()
         }
 
         guard let pattern = bson_iter_regex(&iter.iter, options) else {
--- Sources/MongoSwift/Utils.swift
+++ Sources/MongoSwift/Utils.swift
@@ -1,13 +1,9 @@
-/// An extension to add a custom method for deallocation.
+/// If we are on < 4.1, define our own `deallocate()` that 
+/// calls the old version of the method.
+#if !swift(>=4.1)
 internal extension UnsafeMutablePointer {
-	/// `deallocate(capacity:)` is deprecated as of Swift 4.1, but its replacement
-	/// `deallocate()` doesn't exist in < 4.1. this method calls the correct variant
-	/// based on swift version.
-	internal func versionCheckDeallocate() {
-		#if swift(>=4.1)
-		self.deallocate()
-		#else
+	internal func deallocate() {
 		self.deallocate(capacity: 1)
-		#endif
 	}
 }
+#endif
