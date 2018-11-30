2ab2377efedbd8aa305331620cc8b7e489c426c3
Kaitlin Mahar <kaitlinmahar@gmail.com>
2018-11-30 11:17:55 -0500

    comment

--- Tests/MongoSwiftTests/TestUtils.swift
+++ Tests/MongoSwiftTests/TestUtils.swift
@@ -6,7 +6,7 @@ import XCTest
 class MongoSwiftTestCase: XCTestCase {
     /* Ensure libmongoc is initialized. This will be called multiple times, but that's ok
      * as repeated calls have no effect. There is no way to call final cleanup code just
-     * once at the very end, either explicitly or with a deinit). This may appear as a 
+     * once at the very end, either explicitly or with a deinit. This may appear as a 
      * memory leak. */
     override class func setUp() {
         MongoSwift.initialize()
