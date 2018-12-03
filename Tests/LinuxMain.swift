dcfa05fdf34eba38c67f24a2d0a2cec831bfc572
Kaitlin Mahar <kaitlinmahar@gmail.com>
2018-12-03 11:12:40 -0500

    SWIFT-119 Initialize libmongoc before running tests (#153)

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,11 +1,5 @@
-import MongoSwift
 import MongoSwiftTests
 import XCTest
-
-/* Ensure libmongoc is initialized. Since XCTMain never returns, we have no
- * opportunity to cleanup libmongoc (either explicitly or with a deinit). This
- * may appear as a memory leak. */
-MongoSwift.initialize()
 
 var tests = [XCTestCaseEntry]()
 tests += MongoSwiftTests.allTests()
--- Tests/MongoSwiftTests/TestUtils.swift
+++ Tests/MongoSwiftTests/TestUtils.swift
@@ -4,6 +4,14 @@ import Nimble
 import XCTest
 
 class MongoSwiftTestCase: XCTestCase {
+    /* Ensure libmongoc is initialized. This will be called multiple times, but that's ok
+     * as repeated calls have no effect. There is no way to call final cleanup code just
+     * once at the very end, either explicitly or with a deinit. This may appear as a 
+     * memory leak. */
+    override class func setUp() {
+        MongoSwift.initialize()
+    }
+
     /// Gets the path of the directory containing spec files, depending on whether
     /// we're running from XCode or the command line
     static var specsPath: String {
