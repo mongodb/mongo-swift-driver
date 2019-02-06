6f5fab46119e746afa08dc3b6a166c02509c07ab
Patrick Freed <patrick.freed@mongodb.com>
2019-02-06 17:25:08 -0500

    dont gobble command error messages when tearing down bulk write tests

--- Tests/MongoSwiftTests/MongoCollection+BulkWriteTests.swift
+++ Tests/MongoSwiftTests/MongoCollection+BulkWriteTests.swift
@@ -44,9 +44,8 @@ final class MongoCollection_BulkWriteTests: MongoSwiftTestCase {
     override func tearDown() {
         do {
             try coll.drop()
-        } catch let ServerError.commandError(code, _, _) {
-            // Only ignore NamespaceNotFound errors.
-            expect(code).to(equal(26))
+        } catch let ServerError.commandError(code, _, _) where code == 26 {
+            // ignore ns not found errors
         } catch {
             fail("encountered error when tearing down: \(error)")
         }
