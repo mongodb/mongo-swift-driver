7ec0538e1937d16f9596e008090885763ae2e43e
Matt Broadstone <mbroadst@gmail.com>
2019-04-01 09:49:21 -0400

    use internal `_bson_get_len` from swift C bson bindings
    This method is introduced to overcome limitations around field
    access when `bson_t` is treated as an `OpaquePointer`.

--- Sources/MongoSwift/BSON/Document.swift
+++ Sources/MongoSwift/BSON/Document.swift
@@ -283,7 +283,11 @@ extension Document {
     public var rawBSON: Data {
         // swiftlint:disable:next force_unwrapping - documented as always returning a value.
         let data = bson_get_data(self.data)!
+#if compiler(>=5.0)
+        let length = _bson_get_len(self.data)
+#else
         let length = self.data.pointee.len
+#endif
         return Data(bytes: data, count: Int(length))
     }
 
