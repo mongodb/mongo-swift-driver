8284afb5f2369bbb27dca32dc8444a1f21017ebf
Matt Broadstone <mbroadst@gmail.com>
2019-04-09 10:12:10 -0400

    Update to Nimble 8.x

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/mongodb/swift-bson", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/mongodb/swift-mongoc", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "7.3.0")
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0"))
     ],
     targets: [
         .target(name: "MongoSwift", dependencies: ["mongoc", "bson"]),
