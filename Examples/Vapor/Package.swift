68b99cb96164f26bfd1bfbe23d77d40130c3722d
Kaitlin Mahar <kaitlinmahar@gmail.com>
2019-04-18 16:46:01 -0400

    Vapor import

--- Examples/Vapor/Package.swift
+++ Examples/Vapor/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "VaporExample",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "3.3.0")),
-        .package(url: "https://github.com/mongodb/mongo-swift-driver", .branch("master"))
+        .package(url: "https://github.com/mongodb/mongo-swift-driver", .upToNextMajor(from: "0.1.0"))
     ],
     targets: [
         .target(name: "VaporExample", dependencies: ["Vapor", "MongoSwift"])
