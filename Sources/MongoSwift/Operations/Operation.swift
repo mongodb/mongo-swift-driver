318954ce21c296f5e48d19346769554375b3b099
Kaitlin Mahar <kaitlinmahar@gmail.com>
2019-04-18 21:16:45 -0400

    SWIFT-373: Introduce Operation protocol, CountOperation, and DistinctOperation  (#245)

---
+++ Sources/MongoSwift/Operations/Operation.swift
@@ -0,0 +1,8 @@
+/// A protocol for operation types to conform to. An `Operation` instance corresponds to any single operation a user
+/// can perform with the driver's API that requires I/O.
+internal protocol Operation {
+    /// The result type this operation returns.
+    associatedtype OperationResult
+    /// Executes this operation and returns its corresponding result type.
+    func execute() throws -> OperationResult
+}
