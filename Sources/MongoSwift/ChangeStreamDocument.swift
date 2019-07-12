d2d0b232110e460e858e8c60bda8f93a57b10453
ncarbon <natcarbonell97@gmail.com>
2019-07-12 16:51:17 -0400

    Update docstrings

--- Sources/MongoSwift/ChangeStreamDocument.swift
+++ Sources/MongoSwift/ChangeStreamDocument.swift
@@ -11,13 +11,29 @@ public struct UpdateDescription: Codable {
 
 /// An enum representing the type of operation for this change.
 public enum OperationType: String, Codable {
+   /// Specifies an operation of type `insert`.
+   /// SeeAlso: https://docs.mongodb.com/manual/reference/change-events/index.html#insert-event
    case insert
+   /// Specifies an operation of type `update`.
+   /// SeeAlso: https://docs.mongodb.com/manual/reference/change-events/index.html#update-event
    case update
+   /// Specifies an operation of type `replace`.
+   /// SeeAlso: https://docs.mongodb.com/manual/reference/change-events/index.html#replace-event
    case replace
+   /// Specifies an operation of type `delete`.
+   /// SeeAlso: https://docs.mongodb.com/manual/reference/change-events/index.html#delete-event
    case delete
+   /// Specifies an operation of type `invalidate`.
+   /// SeeAlso: https://docs.mongodb.com/manual/reference/change-events/index.html#change-event-invalidate
    case invalidate
+   /// Specifies an operation of type `drop`.
+   /// SeeAlso: https://docs.mongodb.com/manual/reference/change-events/index.html#drop-event
    case drop
+   /// Specifies an operation of type `dropDatabase`.
+   /// SeeAlso: https://docs.mongodb.com/manual/reference/change-events/index.html#dropdatabase-event
    case dropDatabase
+   /// Specifies an operation of type `rename`.
+   /// SeeAlso: https://docs.mongodb.com/manual/reference/change-events/index.html#rename-event
    case rename
 }
 
@@ -26,21 +42,19 @@ public struct ChangeStreamDocument<T: Codable>: Codable {
     /// Describes the type of operation for this change.
     public let operationType: OperationType
 
-    /// An opaque token for use when resuming an interrupted change
-    /// stream.
+    /// An opaque token for use when resuming an interrupted change stream.
+    /// - SeeAlso: https://docs.mongodb.com/manual/changeStreams/#resume-a-change-stream
     public let _id: ChangeStreamToken
 
     /// A document containing the database and collection names in
     /// which this change happened.
     public let ns: MongoNamespace
 
-    /** Only present for options of type ‘insert’, ‘update’,
-     * ‘replace’ and ‘delete’.
-     *
-     * For unsharded collections this contains a single field, _id, with the
-     * value of the _id of the document updated. For sharded collections,
-     * this will contain all the components of the shard key in order,
-     * followed by the _id if the _id isn’t part of the shard key.
+    /**
+     * Only present for options of type `insert’, `update’, `replace’ and `delete’. For unsharded collections this
+     * contains a single field, _id, with the value of the _id of the document updated. For sharded collections, this
+     * will contain all the components of the shard key in order, followed by the _id if the _id isn’t part of the shard
+     * key.
      */
     public let documentKey: Document?
 
@@ -49,19 +63,15 @@ public struct ChangeStreamDocument<T: Codable>: Codable {
     public let updateDescription: UpdateDescription?
 
    /**
-    * Always present for operations of type ‘insert’ and ‘replace’. Also
-    * present for operations of type ‘update’ if the user has specified
-    * ‘updateLookup’ in the ‘fullDocument’ arguments to the ‘$changeStream’
-    *  stage.
+    * Always present for operations of type `insert` and `replace`. Also present for operations of type `update` if the
+    * user has specified `updateLookup` in the `fullDocument.updateLookup` in the `ChangeStreamOptions` used to create
+    * the `ChangeStream` that emitted this document.
     *
-    * For operations of type ‘insert’ and ‘replace’, this key will contain
-    * the document being inserted, or the new version of the document that is
-    * replacing the existing document, respectively.
+    * For operations of type `insert’ and `replace’, this key will contain the document being inserted, or the new
+    * version of the document that is replacing the existing document, respectively.
     *
-    * For operations of type ‘update’, this key will contain a copy of the
-    * full version of the document from some point after the update occurred.
-    * If the document was deleted since the updated happened, it will be
-    * null.
+    * For operations of type `update’, this key will contain a copy of the full version of the document from some point
+    * after the update occurred. If the document was deleted since the updated happened, it will be null.
     */
     public let fullDocument: T?
  }
