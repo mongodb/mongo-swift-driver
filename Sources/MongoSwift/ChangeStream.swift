63e8513cf8bf9dd15a7fb4c451a4294400db395e
ncarbon <natcarbonell97@gmail.com>
2019-07-12 16:51:17 -0400

    Update docstrings

--- Sources/MongoSwift/ChangeStream.swift
+++ Sources/MongoSwift/ChangeStream.swift
@@ -1,6 +1,8 @@
 import mongoc
 
-/// A `ChangeStreamToken` used for wrapping the resume token.
+/// A token used for manually resuming a change stream. Pass this to the `resumeAfter` or `startAfter` fields of
+/// `ChangeStreamOptions` to resume or start a change stream where a previous one left off.
+/// - SeeAlso: https://docs.mongodb.com/manual/changeStreams/#resume-a-change-stream
 public struct ChangeStreamToken: Codable {
     private let resumeToken: Document
 
@@ -16,7 +18,7 @@ public struct ChangeStreamToken: Codable {
 /// A MongoDB ChangeStream.
 /// - SeeAlso: https://docs.mongodb.com/manual/changeStreams/
 public class ChangeStream<T: Codable>: Sequence, IteratorProtocol {
-  /// The cached resume token.
+  /// A `ChangeStreamToken` used to manually resume a change stream.
   public private(set) var resumeToken: ChangeStreamToken
 
   /// A `MongoClient` stored to make sure the source client stays valid
@@ -33,9 +35,8 @@ public class ChangeStream<T: Codable>: Sequence, IteratorProtocol {
   /// Used for storing Swift errors.
   private var swiftError: Error?
 
-  /// The error that occurred while iterating the ChangeStream cursor,
-  /// if one exists. This should be used to check for errors after
-  /// `next()` returns `nil`.
+  /// The error that occurred while iterating the ChangeStream cursor, if one exists.
+  /// This should be used to check for errors after `next()` returns `nil`.
   public var error: Error? {
     if let err = self.swiftError {
         return err
@@ -68,26 +69,25 @@ public class ChangeStream<T: Codable>: Sequence, IteratorProtocol {
   /// Decoder for decoding documents into type `T`.
   internal let decoder: BSONDecoder
 
-  /// Returns the next `T` in the change stream or nil if there is no next value.
-  /// Will block for a maximum of `maxAwaitTimeMS` milliseconds as specified in the
-  /// `ChangeStreamOptions`, or for the server default timeout if omitted.
+  /// Returns the next `T` in the change stream or nil if there is no next value. Will block for a maximum of
+  /// `maxAwaitTimeMS` milliseconds as specified in the `ChangeStreamOptions`, or for the server default timeout
+  /// if omitted.
   public func next() -> T? {
     let cursor = self.changeStream
     // Allocate space for a reference to a BSON pointer.
     let out = UnsafeMutablePointer<BSONPointer?>.allocate(capacity: 1)
+    defer {
+        out.deinitialize(count: 1)
+        out.deallocate()
+    }
 
     // Check that there’s another document in the stream.
     guard mongoc_change_stream_next(cursor, out) else {
       return nil
     }
 
-    defer {
-        out.deinitialize(count: 1)
-        out.deallocate()
-    }
-
     guard let pointee = out.pointee else {
-        fatalError("mongoc_change_stream_next returned true, but document is nil")
+        fatalError("Could not advance the change stream.")
     }
 
     // we have to copy because libmongoc owns the pointer.
@@ -102,30 +102,25 @@ public class ChangeStream<T: Codable>: Sequence, IteratorProtocol {
     }
 
     do {
-      return try decoder.decode(T.self, from: doc)
+        return try decoder.decode(T.self, from: doc)
     } catch {
-      self.swiftError = error
+        self.swiftError = error
     }
 
     return nil
   }
 
   /**
-   * Returns the next `T` in this change stream or
-   * `nil`, or throws an error if one occurs -- compared to `next()`,
-   * which returns `nil` and requires manually checking for an error
-   * afterward.
-   * Will block for a maximum of `maxAwaitTimeMS` milliseconds as specified in the
-   * `ChangeStreamOptions`, or for the server default timeout if omitted.
-   * - Returns: the next `T` in this change stream,
-   *            or `nil` if at the end of the change stream cursor.
+   * Returns the next `T` in this change stream or `nil`, or throws an error if one occurs -- compared to `next()`,
+   * whichreturns `nil` and requires manually checking for an error afterward. Will block for a maximum of
+   * `maxAwaitTimeMS` milliseconds as specified in the `ChangeStreamOptions`, or for the server default timeout if
+   * omitted.
+   * - Returns: the next `T` in this change stream, or `nil` if at the end of the change stream cursor.
    * - Throws:
-   *   - `ServerError.commandError` if an error occurs on the
-   *     server while iterating the change stream cursor.
-   *   - `UserError.logicError` if this function is called and the
-   *     session associated with this change stream is inactive.
-   *   - `DecodingError` if an error occurs decoding the server's
-   *     response.
+   *   - `ServerError.commandError` if an error occurs on the server while iterating the change stream cursor.
+   *   - `UserError.logicError` if this function is called and the session associated with this change stream is
+   *     inactive.
+   *   - `DecodingError` if an error occurs decoding the server's response.
    */
   public func nextOrError() throws -> T? {
     if let next = self.next() {
@@ -141,10 +136,8 @@ public class ChangeStream<T: Codable>: Sequence, IteratorProtocol {
   /**
    * Initializes a `ChangeStream`.
    * - Throws:
-   *   - `ServerError.commandError` if an error occurred on the
-   *     server when creating the `mongoc_change_stream_t`.
-   *   - `UserError.invalidArgumentError` if the `mongoc_change_stream_t`
-   *     was created with invalid options.
+   *   - `ServerError.commandError` if an error occurred on the server when creating the `mongoc_change_stream_t`.
+   *   - `UserError.invalidArgumentError` if the `mongoc_change_stream_t` was created with invalid options.
    */
   internal init(stealing changeStream: OpaquePointer,
                 client: MongoClient,
@@ -163,12 +156,12 @@ public class ChangeStream<T: Codable>: Sequence, IteratorProtocol {
 
   /// Cleans up internal state.
   deinit {
-     guard let cursor = self.changeStream else {
-          return
-      }
-      mongoc_change_stream_destroy(changeStream)
-      self.changeStream = nil
-      self.session = nil
-      self.client = nil
+    guard let cursor = self.changeStream else {
+        return
+    }
+    mongoc_change_stream_destroy(changeStream)
+    self.changeStream = nil
+    self.session = nil
+    self.client = nil
   }
 }
--- Sources/MongoSwift/ChangeStreamOptions.swift
+++ Sources/MongoSwift/ChangeStreamOptions.swift
@@ -30,26 +30,24 @@ public enum FullDocument: RawRepresentable, Codable {
 
 /// Options to use when creating a `ChangeStream`.
 public struct ChangeStreamOptions: Codable {
-    /** Indicates how the `fullDocument` field of a `ChangeStreamDocument` should
-     * be filled out by the server.
-     * By default (indicated by a nil value for this option), the fullDocument field
-     * in the change stream document will always be present in the case of 'insert'
-     * and 'replace' operations (containing the document being inserted) and will be
-     * nil for all other operations.
+    /**
+     * Indicates how the `fullDocument` field of a `ChangeStreamDocument` should be filled out by the server.
+     * By default (indicated by a nil value for this option), the fullDocument field in the change stream document
+     * will always be present in the case of 'insert' and 'replace' operations (containing the document being inserted)
+     * and will be nil for all other operations.
      */
     public let fullDocument: FullDocument?
 
-    /// Specifies the logical starting point for the new change stream.
-    public let resumeAfter: Document?
+    /// A `ChangeStreamToken` used to manually specify the logical starting point for the new change stream.
+    public let resumeAfter: ChangeStreamToken?
 
     /// The maximum amount of time in milliseconds for the server to wait on new documents
     // to satisfy a change stream query. Uses the server default timeout when omitted.
     public let maxAwaitTimeMS: Int64?
 
     /**
-     * The number of documents to return per batch.
-     * This option is sent only if the caller explicitly provides a value. The
-     * default is to not send a value.
+     * The number of documents to return per batch. This option is sent only if the caller explicitly provides a value.
+     * The default is to not send a value.
      * - SeeAlso: https://docs.mongodb.com/manual/reference/command/aggregate
      */
     public let batchSize: Int32?
@@ -71,8 +69,8 @@ public struct ChangeStreamOptions: Codable {
     public let startAtOperationTime: Timestamp?
 
     /**
-     * Similar to `resumeAfter`, this option takes a resume token and starts a
-     * new change stream returning the first notification after the token.
+     * Similar to `resumeAfter`, this option takes a resume token and starts a new change stream returning the first
+     * notification after the token.
      * This will allow users to watch collections that have been dropped and
      * recreated or newly renamed collections without missing any
      * notifications.
@@ -80,7 +78,7 @@ public struct ChangeStreamOptions: Codable {
      * both specified.
      * - SeeAlso: https://docs.mongodb.com/master/changeStreams/#change-stream-start-after
      */
-    public let startAfter: Document?
+    public let startAfter: ChangeStreamToken?
 
     /// Initializes a `ChangeStreamOption`.
     public init(fullDocument: FullDocument? = nil,
