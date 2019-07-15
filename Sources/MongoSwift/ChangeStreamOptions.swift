275be0a5c361751d15fd7ada1405a6135566baf7
ncarbon <natcarbonell97@gmail.com>
2019-07-15 15:29:08 -0400

    Cleanup after merge

--- Sources/MongoSwift/ChangeStreamOptions.swift
+++ Sources/MongoSwift/ChangeStreamOptions.swift
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 /// Describes the modes for configuring the fullDocument field of a `ChangeStreamDocument`.
 public enum FullDocument: RawRepresentable, Codable {
     /// The change stream document will include both a delta describing the changes to the document,
     /// as well as a copy of the entire document that was changed from some time after the change occurred.
-=======
-/// Describes the modes for configuring the fullDocument field of a
-/// `ChangeStreamDocument`.
-public enum FullDocument: RawRepresentable, Codable {
-    /// The change stream document will include both a delta describing the
-    /// changes to the document, as well as a copy of the entire document that
-    /// was changed from some time after the change occurred.
->>>>>>> first commit - Add ChangeStream, ChangeStreamOptions, ChangeStreamDocument
-=======
-/// Describes the modes for configuring the fullDocument field of a `ChangeStreamDocument`.
-public enum FullDocument: RawRepresentable, Codable {
-    /// The change stream document will include both a delta describing the changes to the document,
-    /// as well as a copy of the entire document that was changed from some time after the change occurred.
->>>>>>> Update docstrings and use ChangeStreamToken for resumeAfter and startAfter
     case updateLookup
     /// For an unknown value. For forwards compatibility, no error will be
     /// thrown when an unknown value is provided.
@@ -44,17 +28,11 @@ public enum FullDocument: RawRepresentable, Codable {
 
 /// Options to use when creating a `ChangeStream`.
 public struct ChangeStreamOptions: Codable {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Update docstrings
     /**
      * Indicates how the `fullDocument` field of a `ChangeStreamDocument` should be filled out by the server.
      * By default (indicated by a nil value for this option), the fullDocument field in the change stream document
      * will always be present in the case of 'insert' and 'replace' operations (containing the document being inserted)
      * and will be nil for all other operations.
-<<<<<<< HEAD
      */
     public let fullDocument: FullDocument?
 
@@ -88,70 +66,11 @@ public struct ChangeStreamOptions: Codable {
     /// Initializes a `ChangeStreamOption`.
     public init(fullDocument: FullDocument? = nil,
                 resumeAfter: ChangeStreamToken? = nil,
-=======
-    /// Indicates the value of the mode on the `fullDocument` field of a
-    /// `ChangeStreamDocument`.
-=======
-    /** Indicates how the `fullDocument` field of a `ChangeStreamDocument` should
-     * be filled out by the server.
-     * By default (indicated by a nil value for this option), the fullDocument field
-     * in the change stream document will always be present in the case of 'insert'
-     * and 'replace' operations (containing the document being inserted) and will be
-     * nil for all other operations.
-=======
->>>>>>> Update docstrings
-     */
->>>>>>> update docstrings
-    public let fullDocument: FullDocument?
-
-    /// A `ChangeStreamToken` to manually specify the resumeToken which will be used to start a new change stream that
-    /// will return the first notification after this token.
-    public let resumeAfter: ChangeStreamToken?
-
-    /// The maximum amount of time in milliseconds for the server to wait on new documents to satisfy a
-    // change stream query. Uses the server default timeout when omitted.
-    public let maxAwaitTimeMS: Int64?
-
-    /// The number of documents to return per batch. The default is to not send a value.
-    /// - SeeAlso: https://docs.mongodb.com/manual/reference/command/aggregate
-    public let batchSize: Int32?
-
-    /// Specifies a collation.
-    /// - SeeAlso: https://docs.mongodb.com/manual/reference/command/aggregate
-    public let collation: Document?
-
-    /// The change stream will only provide changes that occurred at or after the specified timestamp.
-    /// Any command run against the server will return an operation time that can be used here.
-    /// - SeeAlso: https://docs.mongodb.com/manual/reference/method/db.runCommand/
-    public let startAtOperationTime: Timestamp?
-
-    /// A `ChangeStreamToken` similar to `resumeAfter` except `startAfter` will allow users to watch collections
-    /// have been dropped and recreated or newly renamed collections without missing any notifications.
-    /// The server will report an error if `startAfter` and `resumeAfter` are both specified.
-    /// - SeeAlso: https://docs.mongodb.com/master/changeStreams/#change-stream-start-after
-    public let startAfter: ChangeStreamToken?
-
-    /// Initializes a `ChangeStreamOption`.
-    public init(fullDocument: FullDocument? = nil,
-<<<<<<< HEAD
-                resumeAfter: Document? = nil,
->>>>>>> first commit - Add ChangeStream, ChangeStreamOptions, ChangeStreamDocument
-=======
-                resumeAfter: ChangeStreamToken? = nil,
->>>>>>> Update docstrings and use ChangeStreamToken for resumeAfter and startAfter
                 maxAwaitTimeMS: Int64? = nil,
                 batchSize: Int32? = nil,
                 collation: Document? = nil,
                 startAtOperationTime: Timestamp? = nil,
-<<<<<<< HEAD
-<<<<<<< HEAD
                 startAfter: ChangeStreamToken? = nil) {
-=======
-                startAfter: Document? = nil) {
->>>>>>> first commit - Add ChangeStream, ChangeStreamOptions, ChangeStreamDocument
-=======
-                startAfter: ChangeStreamToken? = nil) {
->>>>>>> Update docstrings and use ChangeStreamToken for resumeAfter and startAfter
         self.fullDocument = fullDocument
         self.resumeAfter = resumeAfter
         self.maxAwaitTimeMS = maxAwaitTimeMS
--- Tests/MongoSwiftTests/ChangeStreamTests.swift
+++ Tests/MongoSwiftTests/ChangeStreamTests.swift
@@ -9,12 +9,6 @@ final class ChangeStreamTest: MongoSwiftTestCase {
             print("Skipping test case because of unsupported topology type \(MongoSwiftTestCase.topologyType)")
             return
         }
-    }
-    func testChangeStream() throws {
-        guard MongoSwiftTestCase.topologyType != .single else {
-            print("Skipping test case because of unsupported topology type \(MongoSwiftTestCase.topologyType)")
-            return
-        }
 
         let decoder = BSONDecoder()
         let client = try MongoClient()
@@ -27,7 +21,7 @@ final class ChangeStreamTest: MongoSwiftTestCase {
         let opts = try encodeOptions(options: options, session: session)
         let pipeline: Document = []
 
-        let connection = try client.connectionPool.withConnection { conn in
+        try client.connectionPool.withConnection { conn in
             try db.withMongocDatabase(from: conn) { dbPtr in
                 // TODO: Use MongoDatabase.watch() instead `mongoc_database_watch` of once it gets added
                 let changeStreamPtr: OpaquePointer = mongoc_database_watch(dbPtr, pipeline._bson, opts?._bson)
@@ -45,116 +39,116 @@ final class ChangeStreamTest: MongoSwiftTestCase {
         }
     }
 
-    func testChangeStreamOnADatabase() throws {
-        guard MongoSwiftTestCase.topologyType != .single else {
-            print("Skipping test case because of unsupported topology type \(MongoSwiftTestCase.topologyType)")
-            return
-        }
-        let client = try MongoClient()
+    // func testChangeStreamOnADatabase() throws {
+    //     guard MongoSwiftTestCase.topologyType != .single else {
+    //         print("Skipping test case because of unsupported topology type \(MongoSwiftTestCase.topologyType)")
+    //         return
+    //     }
+    //     let client = try MongoClient()
 
-        if try client.serverVersion() < ServerVersion(major: 4, minor: 0) {
-            print("Skipping test case for server version \(try client.serverVersion())")
-            return
-        }
+    //     if try client.serverVersion() < ServerVersion(major: 4, minor: 0) {
+    //         print("Skipping test case for server version \(try client.serverVersion())")
+    //         return
+    //     }
 
-        let db = client.db(type(of: self).testDatabase)
-        defer { try? db.drop() }
+    //     let db = client.db(type(of: self).testDatabase)
+    //     defer { try? db.drop() }
 
-        let options = ChangeStreamOptions(fullDocument: .updateLookup)
-        let session = try client.startSession()
-        let opts = try encodeOptions(options: options, session: session)
-        let pipeline: Document = []
+    //     let options = ChangeStreamOptions(fullDocument: .updateLookup)
+    //     let session = try client.startSession()
+    //     let opts = try encodeOptions(options: options, session: session)
+    //     let pipeline: Document = []
 
-        try client.connectionPool.withConnection { conn in
-            try db.withMongocDatabase(from: conn) { dbPtr in
-                // TODO: Use MongoDatabase.watch() instead `mongoc_database_watch` of once it gets added
-                let changeStreamPtr: OpaquePointer = mongoc_database_watch(dbPtr, pipeline._bson, opts?._bson)
-                var replyPtr = UnsafeMutablePointer<BSONPointer?>.allocate(capacity: 1)
-                defer {
-                    replyPtr.deinitialize(count: 1)
-                    replyPtr.deallocate()
-                }
+    //     try client.connectionPool.withConnection { conn in
+    //         try db.withMongocDatabase(from: conn) { dbPtr in
+    //             // TODO: Use MongoDatabase.watch() instead `mongoc_database_watch` of once it gets added
+    //             let changeStreamPtr: OpaquePointer = mongoc_database_watch(dbPtr, pipeline._bson, opts?._bson)
+    //             var replyPtr = UnsafeMutablePointer<BSONPointer?>.allocate(capacity: 1)
+    //             defer {
+    //                 replyPtr.deinitialize(count: 1)
+    //                 replyPtr.deallocate()
+    //             }
 
-                let decoder = BSONDecoder()
-                let changeStream = try ChangeStream<ChangeStreamDocument<Document>>(stealing: changeStreamPtr,
-                                                                                    client: client,
-                                                                                    decoder: decoder)
-                // expect the first iteration to be nil since no changes have been made to the database.
-                expect(changeStream.next()).to(beNil())
+    //             let decoder = BSONDecoder()
+    //             let changeStream = try ChangeStream<ChangeStreamDocument<Document>>(stealing: changeStreamPtr,
+    //                                                                                 client: client,
+    //                                                                                 decoder: decoder)
+    //             // expect the first iteration to be nil since no changes have been made to the database.
+    //             expect(changeStream.next()).to(beNil())
 
-                let coll = try db.collection(self.getCollectionName(suffix: "1"))
-                try coll.insertOne(["a": 1], session: session)
+    //             let coll = try db.collection(self.getCollectionName(suffix: "1"))
+    //             try coll.insertOne(["a": 1], session: session)
 
-                // test that the change stream contains a change document for the `insert` operation.
-                let res1 = changeStream.next()
-                expect(res1).toNot(beNil())
-                expect(res1?.operationType).to(equal(.insert))
-                expect(res1?.fullDocument?["a"]).to(bsonEqual(1))
+    //             // test that the change stream contains a change document for the `insert` operation.
+    //             let res1 = changeStream.next()
+    //             expect(res1).toNot(beNil())
+    //             expect(res1?.operationType).to(equal(.insert))
+    //             expect(res1?.fullDocument?["a"]).to(bsonEqual(1))
 
-                // test that the change stream contains a change document for the `drop` operation.
-                try db.drop()
-                let res2 = changeStream.next()
-                expect(res2).toNot(beNil())
-                expect(res2?.operationType).to(equal(.drop))
-            }
-        }
-    }
+    //             // test that the change stream contains a change document for the `drop` operation.
+    //             try db.drop()
+    //             let res2 = changeStream.next()
+    //             expect(res2).toNot(beNil())
+    //             expect(res2?.operationType).to(equal(.drop))
+    //         }
+    //     }
+    // }
 
-    func testChangeStreamOnACollection() throws {
-        guard MongoSwiftTestCase.topologyType != .single else {
-            print("Skipping test case because of unsupported topology type \(MongoSwiftTestCase.topologyType)")
-            return
-        }
-        let client = try MongoClient()
+    // func testChangeStreamOnACollection() throws {
+    //     guard MongoSwiftTestCase.topologyType != .single else {
+    //         print("Skipping test case because of unsupported topology type \(MongoSwiftTestCase.topologyType)")
+    //         return
+    //     }
+    //     let client = try MongoClient()
 
-        let db = client.db(type(of: self).testDatabase)
-        defer { try? db.drop() }
+    //     let db = client.db(type(of: self).testDatabase)
+    //     defer { try? db.drop() }
 
-        // TODO: Use MongoCollection.watch() instead `mongoc_collection_watch` of once it gets added
-        let coll = try db.createCollection(self.getCollectionName(suffix: "1"))
-        let session = try client.startSession()
-        let options = ChangeStreamOptions(fullDocument: .updateLookup)
-        let opts = try encodeOptions(options: options, session: session)
-        let pipeline: Document = []
+    //     // TODO: Use MongoCollection.watch() instead `mongoc_collection_watch` of once it gets added
+    //     let coll = try db.createCollection(self.getCollectionName(suffix: "1"))
+    //     let session = try client.startSession()
+    //     let options = ChangeStreamOptions(fullDocument: .updateLookup)
+    //     let opts = try encodeOptions(options: options, session: session)
+    //     let pipeline: Document = []
 
-        try client.connectionPool.withConnection { conn in
-            try coll.withMongocCollection(from: conn) { collPtr in
-                let changeStreamPtr: OpaquePointer = mongoc_collection_watch(collPtr, pipeline._bson, opts?._bson)
-                var replyPtr = UnsafeMutablePointer<BSONPointer?>.allocate(capacity: 1)
-                defer {
-                    replyPtr.deinitialize(count: 1)
-                    replyPtr.deallocate()
-                }
+    //     try client.connectionPool.withConnection { conn in
+    //         try coll.withMongocCollection(from: conn) { collPtr in
+    //             let changeStreamPtr: OpaquePointer = mongoc_collection_watch(collPtr, pipeline._bson, opts?._bson)
+    //             var replyPtr = UnsafeMutablePointer<BSONPointer?>.allocate(capacity: 1)
+    //             defer {
+    //                 replyPtr.deinitialize(count: 1)
+    //                 replyPtr.deallocate()
+    //             }
 
-                let decoder = BSONDecoder()
-                let changeStream = try ChangeStream<ChangeStreamDocument<Document>>(stealing: changeStreamPtr,
-                                                                                    client: client,
-                                                                                    session: session,
-                                                                                    decoder: decoder)
-                // expect the first iteration to be nil since no changes have been made to the collection.
-                expect(changeStream.next()).to(beNil())
+    //             let decoder = BSONDecoder()
+    //             let changeStream = try ChangeStream<ChangeStreamDocument<Document>>(stealing: changeStreamPtr,
+    //                                                                                 client: client,
+    //                                                                                 session: session,
+    //                                                                                 decoder: decoder)
+    //             // expect the first iteration to be nil since no changes have been made to the collection.
+    //             expect(changeStream.next()).to(beNil())
 
-                // test that the change stream contains a change document for the `insert` operation.
-                try coll.insertOne(["x": 1], session: session)
-                let res1 = changeStream.next()
-                expect(res1).toNot(beNil())
-                expect(res1?.operationType).to(equal(.insert))
-                expect(res1?.fullDocument?["x"]).to(bsonEqual(1))
+    //             // test that the change stream contains a change document for the `insert` operation.
+    //             try coll.insertOne(["x": 1], session: session)
+    //             let res1 = changeStream.next()
+    //             expect(res1).toNot(beNil())
+    //             expect(res1?.operationType).to(equal(.insert))
+    //             expect(res1?.fullDocument?["x"]).to(bsonEqual(1))
 
-                try coll.updateOne(filter: ["x": 1], update: ["$set": ["x": 2] as Document], session: session)
-                let res2 = changeStream.next()
+    //             try coll.updateOne(filter: ["x": 1], update: ["$set": ["x": 2] as Document], session: session)
+    //             let res2 = changeStream.next()
 
-                // test that the change stream contains a change document for the `update` operation.
-                expect(res2).toNot(beNil())
-                expect(res2?.operationType).to(equal(.update))
-                expect(res2?.fullDocument?["x"]).to(bsonEqual(2))
+    //             // test that the change stream contains a change document for the `update` operation.
+    //             expect(res2).toNot(beNil())
+    //             expect(res2?.operationType).to(equal(.update))
+    //             expect(res2?.fullDocument?["x"]).to(bsonEqual(2))
 
-                // test that the change stream contains a change document for the `find` operation.
-                try coll.findOneAndDelete(["x": 2], session: session)
-                let res3 = changeStream.next()
-                expect(res3).toNot(beNil())
-                expect(res3?.operationType).to(equal(.delete))
-            }
-        }
-    }
+    //             // test that the change stream contains a change document for the `find` operation.
+    //             try coll.findOneAndDelete(["x": 2], session: session)
+    //             let res3 = changeStream.next()
+    //             expect(res3).toNot(beNil())
+    //             expect(res3?.operationType).to(equal(.delete))
+    //         }
+    //     }
+    // }
 }
