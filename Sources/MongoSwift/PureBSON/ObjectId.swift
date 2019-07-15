6f2481a52591785d3a8e7f6612071213e41b050a
Kaitlin Mahar <kaitlinmahar@gmail.com>
2019-07-15 19:22:48 -0400

    Add timestamp field and do some cleanup

--- Sources/MongoSwift/PureBSON/ObjectId.swift
+++ Sources/MongoSwift/PureBSON/ObjectId.swift
@@ -1,53 +1,85 @@
 import Foundation
+import bson
 
 // TODO: figure out if Int.random is random enough to satisfy OID spec or if we need to pass in some custom PRNG.
 // also unclear if the second 5 bytes are "process unique".
 
-//   4 byte timestamp    5 byte process unique   3 byte counter
-// |<----------------->|<---------------------->|<------------>|
-// [----|----|----|----|----|----|----|----|----|----|----|----]
-// 0                   4                   8                   12
-
 public struct PureBSONObjectId {
-    private static let counter = ObjectIdCounter()
+    /// Stores the twelve bytes of data comprising this `ObjectId`.
+    ///   4 byte timestamp    5 byte process unique   3 byte counter
+    /// |<----------------->|<---------------------->|<------------>|
+    /// [----|----|----|----|----|----|----|----|----|----|----|----]
+    /// 0                   4                   8                   12
     internal let data: Data
+    /// Counter used to generate the last three bytes of each `ObjectId`.
+    private static let counter = ObjectIdCounter()
 
+    /// The timestamp used to create this `ObjectId`, represented as seconds since the Unix epoch.
+    public let timestamp: UInt32
+
+    /// This ObjectId as a hexadecimal string.
+    public var hex: String {
+        return self.data.reduce("") { $0 + String(format: "%02x", $1) }
+    }
+
+    /// Initializes a new `ObjectId`.
     public init() {
         var data = Data(capacity: 12)
-        // append most significant 4 bytes
+
         let secondsSinceEpoch = Date().timeIntervalSince1970
-        let timestamp = UInt32(secondsSinceEpoch).bigEndian
-        withUnsafeBytes(of: timestamp) { bytes in
+        let timestamp = UInt32(secondsSinceEpoch)
+        self.timestamp = timestamp
+        // 4-byte big endian field represents the seconds since the Unix epoch
+        withUnsafeBytes(of: timestamp.bigEndian) { bytes in
             data.append(Data(bytes))
         }
 
-        // generate a unique number that uses at most 5 bytes
-        let processUnique = UInt64.random(in: 0...UInt64(pow(2, 40.0))).bigEndian
+        // generate a random number that uses at most 5 bytes
+        let processUnique = UInt64.random(in: 0...UInt64(pow(2, 40.0)))
         // append the least significant 5 bytes
-        withUnsafeBytes(of: processUnique) { bytes in
+        withUnsafeBytes(of: processUnique.bigEndian) { bytes in
             data.append(Data(bytes[3...7]))
         }
+
+        let counterValue = PureBSONObjectId.counter.next()
         // get the next number from the counter and append the least significant 3 bytes
-        withUnsafeBytes(of: PureBSONObjectId.counter.next().bigEndian) { bytes in
+        withUnsafeBytes(of: counterValue.bigEndian) { bytes in
             data.append(Data(bytes[1...3]))
         }
         self.data = data
     }
 
-    internal var hexDescription: String {
-        return self.data.reduce("") { $0 + String(format: "%02x", $1) }
+    /// Initializes a new `ObjectId` from a hexadecimal string. Returns `nil` if the hex string is invalid.
+    public init?(_ hex: String) {
+        // TODO: parse hex here ourselves rather than letting libmongoc do it.
+        guard bson_oid_is_valid(hex, hex.utf8.count) else {
+            return nil
+        }
+        var oid_t = bson_oid_t()
+        bson_oid_init_from_string(&oid_t, hex)
+        let bytes = [UInt8](UnsafeBufferPointer(start: &oid_t.bytes.0, count: MemoryLayout.size(ofValue: oid_t.bytes)))
+        self.data = Data(bytes)
+        self.timestamp = UInt32(bson_oid_get_time_t(&oid_t))
     }
 }
 
 extension PureBSONObjectId: CustomStringConvertible {
     public var description: String {
-        return self.hexDescription
+        return self.hex
     }
 }
+
+extension PureBSONObjectId: Equatable {}
+extension PureBSONObjectId: Hashable {}
 
 extension PureBSONObjectId: PureBSONValue {
     internal init(from data: Data) throws {
         self.data = data
+        // first four bytes are the timestamp.
+        self.timestamp = UInt32(data[0]) << 24
+                       + UInt32(data[1]) << 16
+                       + UInt32(data[2]) << 8
+                       + UInt32(data[3])
     }
 
     internal func toBSON() -> Data {
@@ -60,11 +92,11 @@ internal class ObjectIdCounter {
     private let queue = DispatchQueue(label: "ObjectId counter queue")
     /// Current count. This variable must only be read and written within `queue.sync` blocks.
     private var count = UInt32.random(in: 0...max)
-
-    private static var max = UInt32(pow(2, 24.0))
+    /// Maximum value this counter can return.
+    private static var max = UInt32(16777215)
 
     /// Returns the next value in the counter.
-    func next() -> UInt32 {
+    internal func next() -> UInt32 {
         return queue.sync {
             self.count += 1
             // When the counter overflows (i.e., hits 16777215+1), the counter MUST be reset to 0.
