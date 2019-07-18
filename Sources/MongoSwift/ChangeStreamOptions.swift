c367087d9a3073e8504f01629d3d5fbb69f82cf0
ncarbon <natcarbonell97@gmail.com>
2019-07-17 23:07:06 -0400

    fix indentation and docstrings

--- Sources/MongoSwift/ChangeStreamOptions.swift
+++ Sources/MongoSwift/ChangeStreamOptions.swift
@@ -1,7 +1,7 @@
-/// Describes the modes for configuring the fullDocument field of a `ChangeStreamDocument`.
+/// Describes the modes for configuring the fullDocument field of a change stream document.
 public enum FullDocument: RawRepresentable, Codable {
     /// Specifies that the `fullDocument` field will contain a copy of the entire document that was changed from
-    /// some time after the change occurred. If the document was deleted since the updated happened, it will be null.
+    /// some time after the change occurred. If the document was deleted since the updated happened, it will be nil.
     case updateLookup
     /// For an unknown value. For forwards compatibility, no error will be thrown when an unknown value is provided.
     case other(String)
@@ -28,7 +28,7 @@ public enum FullDocument: RawRepresentable, Codable {
 /// Options to use when creating a `ChangeStream`.
 public struct ChangeStreamOptions: Codable {
     /**
-     * Indicates how the `fullDocument` field of a `ChangeStreamDocument` should be filled out by the server.
+     * Indicates how the `fullDocument` field of a change stream document should be filled out by the server.
      * By default (indicated by a nil value for this option), the `fullDocument` field in the change stream document
      * will always be present in the case of 'insert' and 'replace' operations (containing the document being inserted)
      * and will be nil for all other operations.
@@ -40,16 +40,15 @@ public struct ChangeStreamOptions: Codable {
      * The change stream will attempt to resume notifications starting after the operation associated with
      * the provided token.
      * - Note: A change stream cannot be resumed after an invalidate event (e.g. a collection drop or rename).
-     *         Use the `startAfter` option in those cases instead.
      * - SeeAlso: https://docs.mongodb.com/manual/changeStreams/#resume-a-change-stream
      */
     public let resumeAfter: ChangeStreamToken?
 
     /// The maximum amount of time in milliseconds for the server to wait on new documents to satisfy a
-    /// change stream query. Uses the server default timeout when omitted.
+    /// change stream query. If omitted, the server will use its default timeout.
     public let maxAwaitTimeMS: Int64?
 
-    /// The number of documents to return per batch. The default is to not send a value.
+    /// The number of documents to return per batch. If omitted, the server will use its default batch size.
     /// - SeeAlso: https://docs.mongodb.com/manual/reference/command/aggregate
     public let batchSize: Int32?
 
@@ -70,7 +69,8 @@ public struct ChangeStreamOptions: Codable {
      * - Note: The server will report an error if `startAfter` and `resumeAfter` are both specified.
      * - SeeAlso: https://docs.mongodb.com/master/changeStreams/#change-stream-start-after
      */
-    public let startAfter: ChangeStreamToken?
+     // TODO: Make this public when support is added for 4.2 change stream features.
+    internal let startAfter: ChangeStreamToken?
 
     /// Initializes a `ChangeStreamOptions`.
     public init(fullDocument: FullDocument? = nil,
@@ -78,14 +78,13 @@ public struct ChangeStreamOptions: Codable {
                 maxAwaitTimeMS: Int64? = nil,
                 batchSize: Int32? = nil,
                 collation: Document? = nil,
-                startAtOperationTime: Timestamp? = nil,
-                startAfter: ChangeStreamToken? = nil) {
+                startAtOperationTime: Timestamp? = nil) {
         self.fullDocument = fullDocument
         self.resumeAfter = resumeAfter
         self.maxAwaitTimeMS = maxAwaitTimeMS
         self.batchSize = batchSize
         self.collation = collation
         self.startAtOperationTime = startAtOperationTime
-        self.startAfter = startAfter
+        self.startAfter = nil
     }
 }
