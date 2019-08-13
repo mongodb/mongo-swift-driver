feca03c28bfd3a5ef231f94c1c6646e72e37ef55
Patrick Freed <patrick.freed@mongodb.com>
2019-08-13 15:05:36 -0400

    cr comments

---
+++ Examples/Docs/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "DocsExamples",
+    dependencies: [
+        .package(url: "https://github.com/mongodb/mongo-swift-driver", .upToNextMinor(from: "0.1.0"))
+    ],
+    targets: [
+        .target(name: "DocsExamples", dependencies: ["MongoSwift"])
+    ]
+)
