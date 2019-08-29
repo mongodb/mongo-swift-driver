5850eafc2d008c5bff0508af2506ec59f01cfd04
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2019-08-29 15:08:12 -0400

    SWIFT-170, SWIFT-612: Add ChangeStreams examples for docs (#314)

--- Examples/Docs/Sources/DocsExamples/main.swift
+++ Examples/Docs/Sources/DocsExamples/main.swift
@@ -6,8 +6,9 @@ import MongoSwift
 /// Examples used for the MongoDB documentation on Causal Consistency.
 /// - SeeAlso: https://docs.mongodb.com/manual/core/read-isolation-consistency-recency/#examples
 private func causalConsistency() throws {
-    // Start example 1
     let client1 = try MongoClient()
+
+    // Start Causal Consistency Example 1
     let s1 = try client1.startSession(options: ClientSessionOptions(causalConsistency: true))
     let currentDate = Date()
     var dbOptions = DatabaseOptions(readConcern: ReadConcern(.majority),
@@ -17,9 +18,11 @@ private func causalConsistency() throws {
                         update: ["$set": ["end": currentDate] as Document],
                         session: s1)
     try items.insertOne(["sku": "nuts-111", "name": "Pecans", "start": currentDate], session: s1)
+    // End Causal Consistency Example 1
 
-    // Start example 2
     let client2 = try MongoClient()
+
+    // Start Causal Consistency Example 2
     try client2.withSession(options: ClientSessionOptions(causalConsistency: true)) { s2 in
         // The cluster and operation times are guaranteed to be non-nil since we already used s1 for operations above.
         s2.advanceClusterTime(to: s1.clusterTime!)
@@ -30,5 +33,56 @@ private func causalConsistency() throws {
         for item in try items2.find(["end": BSONNull()], session: s2) {
             print(item)
         }
+    }
+    // End Causal Consistency Example 2
+}
+
+/// Examples used for the MongoDB documentation on Change Streams.
+/// - SeeAlso: https://docs.mongodb.com/manual/changeStreams/
+private func changeStreams() throws {
+    let client = try MongoClient()
+    let db = client.db("example")
+
+    // The following examples assume that you have connected to a MongoDB replica set and have
+    // accessed a database that contains an inventory collection.
+
+    do {
+        // Start Changestream Example 1
+        let inventory = db.collection("inventory")
+        let cursor = try inventory.watch()
+        let next = try cursor.nextOrError()
+        // End Changestream Example 1
+    }
+
+    do {
+        // Start Changestream Example 2
+        let inventory = db.collection("inventory")
+        let cursor = try inventory.watch(options: ChangeStreamOptions(fullDocument: .updateLookup))
+        let next = try cursor.nextOrError()
+        // End Changestream Example 2
+    }
+
+    do {
+        // Start Changestream Example 3
+        let inventory = db.collection("inventory")
+        let cursor = try inventory.watch(options: ChangeStreamOptions(fullDocument: .updateLookup))
+        let next = try cursor.nextOrError()
+
+        let resumeToken = cursor.resumeToken
+        let resumedCursor = try inventory.watch(options: ChangeStreamOptions(resumeAfter: resumeToken))
+        let nextAfterResume = try resumedCursor.nextOrError()
+        // End Changestream Example 3
+    }
+
+    do {
+        // Start Changestream Example 4
+        let pipeline: [Document] = [
+            ["$match": ["fullDocument.username": "alice"] as Document],
+            ["$addFields": ["newField": "this is an added field!"] as Document]
+        ]
+        let inventory = db.collection("inventory")
+        let cursor = try inventory.watch(pipeline, withEventType: Document.self)
+        let next = try cursor.nextOrError()
+        // End Changestream Example 4
     }
 }
