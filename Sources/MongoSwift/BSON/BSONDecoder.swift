b492c0b49341d1956f0f14f4b1eb2cd3ced1dc78
Katherine Wu <katherine.wu@mongodb.com>
2019-09-23 14:55:54 -0400

    explicit_acl linter

--- Sources/MongoSwift/BSON/BSONDecoder.swift
+++ Sources/MongoSwift/BSON/BSONDecoder.swift
@@ -2,6 +2,8 @@ import Foundation
 
 /// `BSONDecoder` facilitates the decoding of BSON into semantic `Decodable` types.
 public class BSONDecoder {
+    // swiftlint:disable explicit_acl
+    // some kind of swiftlint bug here.
     @available(macOS 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)
     internal static var iso8601Formatter: ISO8601DateFormatter = {
         let formatter = ISO8601DateFormatter()
