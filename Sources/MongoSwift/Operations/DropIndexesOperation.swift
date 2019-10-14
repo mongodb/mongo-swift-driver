1be6e4ec06ad17f1d6e47ca0923a0769c0dde7c6
Kaitlin Mahar <kaitlinmahar@gmail.com>
2019-10-13 21:43:58 -0400

    Update DropIndexesOperation.swift

--- Sources/MongoSwift/Operations/DropIndexesOperation.swift
+++ Sources/MongoSwift/Operations/DropIndexesOperation.swift
@@ -1,6 +1,6 @@
 import mongoc
 
-/// Options to use when dropping an index from a `MongoCollection`.
+/// Options to use when dropping an index from a `MongoCollection` or a `SyncMongoCollection`.
 public struct DropIndexOptions: Encodable {
     /// The maximum amount of time to allow the query to run - enforced server-side.
     public var maxTimeMS: Int64?
@@ -17,17 +17,17 @@ public struct DropIndexOptions: Encodable {
 
 /// An operation corresponding to a "dropIndexes" command.
 internal struct DropIndexesOperation<T: Codable>: Operation {
-    private let collection: MongoCollection<T>
+    private let collection: SyncMongoCollection<T>
     private let index: BSONValue
     private let options: DropIndexOptions?
 
-    internal init(collection: MongoCollection<T>, index: BSONValue, options: DropIndexOptions?) {
+    internal init(collection: SyncMongoCollection<T>, index: BSONValue, options: DropIndexOptions?) {
         self.collection = collection
         self.index = index
         self.options = options
     }
 
-    internal func execute(using connection: Connection, session: ClientSession?) throws -> Document {
+    internal func execute(using connection: Connection, session: SyncClientSession?) throws -> Document {
         let command: Document = ["dropIndexes": self.collection.name, "index": self.index]
         let opts = try encodeOptions(options: self.options, session: session)
         var reply = Document()
