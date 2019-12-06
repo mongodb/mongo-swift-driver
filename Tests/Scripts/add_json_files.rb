a29382b5f8c7466b0fbd667d84e47b3210f5ae2b
Kaitlin Mahar <kaitlinmahar@gmail.com>
2019-12-05 19:01:02 -0500

    fix xcodebuild use

--- Tests/Scripts/add_json_files.rb
+++ Tests/Scripts/add_json_files.rb
@@ -10,8 +10,9 @@ def make_reference(project, path)
     return fileRef
 end
 
-mongoswift_tests_target = targets.find { |t| t.uuid == "MongoSwift::MongoSwiftTests" }
+mongoswift_tests_target = targets.find { |t| t.uuid == "MongoSwift::TestsCommon" }
 crud = make_reference(project, "./Tests/Specs/crud")
+corpus = make_reference(project, "./Tests/Specs/bson-corpus")
 cm = make_reference(project, "./Tests/Specs/command-monitoring")
 read_write_concern = make_reference(project, "./Tests/Specs/read-write-concern")
 retryable_writes = make_reference(project, "./Tests/Specs/retryable-writes")
@@ -19,10 +20,6 @@ retryable_reads = make_reference(project, "./Tests/Specs/retryable-reads")
 change_streams = make_reference(project, "./Tests/Specs/change-streams")
 dns_seedlist = make_reference(project, "./Tests/Specs/initial-dns-seedlist-discovery")
 auth = make_reference(project, "./Tests/Specs/auth")
-mongoswift_tests_target.add_resources([crud, cm, read_write_concern, retryable_writes, retryable_reads, change_streams, dns_seedlist, auth])
-
-bson_tests_target = targets.find { |t| t.uuid == "MongoSwift::BSONTests" }
-corpus = make_reference(project, "./Tests/Specs/bson-corpus")
-bson_tests_target.add_resources([corpus])
+mongoswift_tests_target.add_resources([crud, corpus, cm, read_write_concern, retryable_writes, retryable_reads, change_streams, dns_seedlist, auth])
 
 project.save
