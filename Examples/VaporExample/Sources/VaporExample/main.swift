66c82ec2efe99d5709821b0f5c9df6acd4b7a962
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2019-12-10 16:54:23 -0500

    Don't share collections across threads in example projects (#367)

--- Examples/VaporExample/Sources/VaporExample/main.swift
+++ Examples/VaporExample/Sources/VaporExample/main.swift
@@ -7,14 +7,13 @@ private struct Kitten: Content {
     var color: String
 }
 
-/// A single collection with type `Kitten`. This allows us to directly retrieve instances of
-/// `Kitten` from the collection.  `MongoCollection` is safe to share across threads.
-private let collection = try MongoClient().db("home").collection("kittens", withType: Kitten.self)
-
 private let app = try Application()
 private let router = try app.make(Router.self)
 
 router.get("kittens") { _ -> [Kitten] in
+    /// A single collection with type `Kitten`. This allows us to directly retrieve instances of
+    /// `Kitten` from the collection.
+    let collection = try MongoClient().db("home").collection("kittens", withType: Kitten.self)
     let docs = try collection.find()
     return Array(docs)
 }
