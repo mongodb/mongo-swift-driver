39c288ece03134ab4d98d4fbec897615d1e09ca5
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-01-31 19:46:21 -0500

    run tests with all env vars

--- Sources/AtlasConnectivity/main.swift
+++ Sources/AtlasConnectivity/main.swift
@@ -8,7 +8,7 @@ for config in configs + srvConfigs {
     print("Testing config \(config)... ", terminator: "")
 
     guard let uri = ProcessInfo.processInfo.environment[config] else {
-        print("Failed: couldn't find URI")
+        print("Failed: couldn't find URI for config \(config)")
         exit(1)
     }
 
