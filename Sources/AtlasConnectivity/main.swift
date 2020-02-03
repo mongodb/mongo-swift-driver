3486d9ae844ae15c670aaccf6ae7732f56184cbd
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2020-02-03 13:58:53 -0500

    SWIFT-703 Re-enable Atlas tests (#396)

--- Sources/AtlasConnectivity/main.swift
+++ Sources/AtlasConnectivity/main.swift
@@ -2,12 +2,13 @@ import Foundation
 import MongoSwiftSync
 
 private let configs = ["ATLAS_REPL", "ATLAS_SHRD", "ATLAS_FREE", "ATLAS_TLS11", "ATLAS_TLS12"]
+private let srvConfigs = configs.map { $0 + "_SRV" }
 
-for config in configs {
+for config in configs + srvConfigs {
     print("Testing config \(config)... ", terminator: "")
 
     guard let uri = ProcessInfo.processInfo.environment[config] else {
-        print("Failed: couldn't find URI")
+        print("Failed: couldn't find URI for config \(config)")
         exit(1)
     }
 
@@ -18,8 +19,7 @@ for config in configs {
         _ = try db.runCommand(["isMaster": 1])
         // findOne
         let coll = db.collection("test")
-        let res = try coll.find(options: FindOptions(limit: 1))
-        for _ in res {} // iterate cursor so we actually talk to server
+        _ = try coll.findOne()
     } catch {
         print("Failed: \(error)")
         exit(1)
