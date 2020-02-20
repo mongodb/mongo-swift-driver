6f1b49139630c517c2309b8c30de7c0a9d94d4f1
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-02-19 23:19:26 -0500

    format

--- Examples/VaporExample/Package.swift
+++ Examples/VaporExample/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "VaporExample",
     platforms: [
-       .macOS(.v10_14)
+        .macOS(.v10_14)
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", .exact("4.0.0-beta.3.24")),
