9e18aa1cc093d99a9859153c86f6f48b61d8a20f
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-02-21 14:30:18 -0500

    Add comments re: Vapor 4 support

--- Examples/VaporExample/Package.swift
+++ Examples/VaporExample/Package.swift
@@ -7,8 +7,9 @@ let package = Package(
         .macOS(.v10_14)
     ],
     dependencies: [
+        // The driver depends on SwiftNIO 2 and therefore is only compatible with Vapor 4.
         .package(url: "https://github.com/vapor/vapor", .exact("4.0.0-beta.3.24")),
-        .package(url: "https://github.com/mongodb/mongo-swift-driver", .branch("master"))
+        .package(url: "https://github.com/mongodb/mongo-swift-driver", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(name: "VaporExample", dependencies: ["Vapor", "MongoSwift"])
--- Examples/VaporExample/README.md
+++ Examples/VaporExample/README.md
@@ -2,6 +2,8 @@
 
 This is a minimal working example of using the driver in a Vapor application.
 
+**Note**: Since the driver depends on SwiftNIO 2 as of the 1.0.0-rc0 release, it is only compatible with Vapor 4. 
+
 To test it out, do the following:
 1. Run `mongod` to start MongoDB running on `localhost:27017`.
 1. Navigate to the `Examples/` directory (one level up from this one.)
