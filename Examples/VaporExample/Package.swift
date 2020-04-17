f12fab999ee6ea334cf94db3cb659619cc42f586
Jonny7 <jonathon.cooper7@gmail.com>
2020-04-17 15:53:36 -0300

    updates Package.swift for Vapor 4

--- Examples/VaporExample/Package.swift
+++ Examples/VaporExample/Package.swift
@@ -1,17 +1,20 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "VaporExample",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     dependencies: [
         // The driver depends on SwiftNIO 2 and therefore is only compatible with Vapor 4.
-        .package(url: "https://github.com/vapor/vapor", .exact("4.0.0-beta.3.24")),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.2.1"),
         .package(url: "https://github.com/mongodb/mongo-swift-driver", .upToNextMajor(from: "1.0.0-rc0"))
     ],
     targets: [
-        .target(name: "VaporExample", dependencies: ["Vapor", "MongoSwift"])
+        .target(name: "VaporExample", dependencies: [
+            .product(name: "MongoSwift", package: "mongo-swift-driver"),
+            .product(name: "Vapor", package: "vapor"),
+        ])
     ]
 )
