8cb04f647b2c0738de33ba59fa0a101fbda8925f
Neal Beeken <neal.beeken@mongodb.com>
2020-05-04 11:39:54 -0400

    fix: patrick comments

--- Sources/MongoSwift/MongoCredential.swift
+++ Sources/MongoSwift/MongoCredential.swift
@@ -3,13 +3,17 @@ public struct MongoCredential: Decodable, Equatable {
     /// A string containing the username. For auth mechanisms that do not utilize a password, this may be the entire
     /// `userinfo` token from the connection string.
     public var username: String?
+
     /// A string containing the password.
     public var password: String?
+
     /// A string containing the authentication database.
     public var source: String?
+
     /// The authentication mechanism. A nil value for this property indicates that a mechanism wasn't specified and
     /// that mechanism negotiation is required.
     public var mechanism: Mechanism?
+
     /// A document containing mechanism-specific properties.
     public var mechanismProperties: Document?
 
