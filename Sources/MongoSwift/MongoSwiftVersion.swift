8fd44764b33c32473afb67d38b6f63d1865fd996
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-05-05 14:40:22 -0400

    1.0.0-rc1

--- Sources/MongoSwift/MongoSwiftVersion.swift
+++ Sources/MongoSwift/MongoSwiftVersion.swift
@@ -1,4 +1,6 @@
 // Generated using Sourcery 0.16.1 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
-internal let MongoSwiftVersionString = "1.0.0-rc0"
+
+// swiftlint:disable:previous vertical_whitespace
+internal let MongoSwiftVersionString = "1.0.0-rc1"
