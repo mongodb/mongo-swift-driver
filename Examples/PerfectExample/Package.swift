ccdfa50d2c73ec6fb2ab285a1a9f09d787a597f3
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-05-26 20:50:51 -0400

    Update driver dependencies

--- Examples/PerfectExample/Package.swift
+++ Examples/PerfectExample/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "PerfectExample",
     dependencies: [
         .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-        .package(url: "https://github.com/mongodb/mongo-swift-driver", .upToNextMajor(from: "1.0.0-rc0")),
+        .package(url: "https://github.com/mongodb/mongo-swift-driver", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.14.0"))
     ],
     targets: [
