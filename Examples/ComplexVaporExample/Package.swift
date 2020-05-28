caf705606c95f09e9a0a6b492699f85e07f3c6c8
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-05-28 13:07:22 -0400

    Create app template

---
+++ Examples/ComplexVaporExample/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "ComplexVaporExample",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "4.7.0")),
+        // 🍃 MongoDB driver.
+        .package(url: "https://github.com/mongodb/mongo-swift-driver", .upToNextMajor(from: "1.0.0-rc1"))
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "MongoSwift", package: "mongo-swift-driver")
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .target(name: "Run", dependencies: [.target(name: "App")]),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
---
+++ Examples/ComplexVaporExample/Tests/AppTests/AppTests.swift
@@ -0,0 +1,16 @@
+@testable import App
+import XCTVapor
+
+final class AppTests: XCTestCase {
+    func testHelloWorld() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "hello") { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Hello, world!")
+        }
+    }
+}
+
