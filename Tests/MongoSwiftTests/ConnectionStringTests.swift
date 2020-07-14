9a05e601a2a20e793e77523b7bf044b8ab31b0c2
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-07-14 13:29:03 -0400

    Raise threshold

--- Tests/MongoSwiftTests/ConnectionStringTests.swift
+++ Tests/MongoSwiftTests/ConnectionStringTests.swift
@@ -345,6 +345,6 @@ final class ConnectionStringTests: MongoSwiftTestCase {
         let secondToLastSuccess = succeeded[succeeded.count - 2]
 
         let difference = lastStart.timeIntervalSince1970 - secondToLastSuccess.timeIntervalSince1970
-        expect(difference).to(beCloseTo(2.0, within: 0.01))
+        expect(difference).to(beCloseTo(2.0, within: 0.2))
     }
 }
