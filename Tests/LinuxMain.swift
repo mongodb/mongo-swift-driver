00b29fdc1684be76f2de51b27bf3b1acbe2a502f
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-07-16 16:25:47 -0400

    Update LinuxMain.swift

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -100,6 +100,8 @@ extension ConnectionStringTests {
         ("testServerSelectionTimeoutMS", testServerSelectionTimeoutMS),
         ("testServerSelectionTimeoutMSWithCommand", testServerSelectionTimeoutMSWithCommand),
         ("testLocalThresholdMSOption", testLocalThresholdMSOption),
+        ("testMinPoolSizeErrors", testMinPoolSizeErrors),
+        ("testCompressionOptions", testCompressionOptions),
     ]
 }
 
