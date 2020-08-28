5703fb5dd6cefd8500eaaad3baaaa935b14500b5
Patrick Freed <patrick.freed@mongodb.com>
2020-08-27 21:39:35 -0400

    use mongodb repo

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
         .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.15.0")),
-        .package(url: "https://github.com/patrickfreed/swift-bson", .branch("target-rename"))
+        .package(url: "https://github.com/mongodb/swift-bson", .branch("master"))
     ],
     targets: [
         .target(name: "MongoSwift", dependencies: ["CLibMongoC", "NIO", "NIOConcurrencyHelpers", "SwiftBSON",]),
