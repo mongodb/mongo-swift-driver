c8e2b16b462355db96b452cf33da546e283cae36
Patrick Freed <patrick.freed@mongodb.com>
2020-09-17 13:11:22 -0400

    fix typo

--- Sources/MongoSwift/ConnectionString.swift
+++ Sources/MongoSwift/ConnectionString.swift
@@ -8,7 +8,7 @@ internal class ConnectionString {
     private var destroyedPtr = false
 
     /// Minimum possible value for a heartbeatFrequencyMS specified via the URI or options.
-    /// This may be overriden in the tests via an internal option on `MongoClientOptions`.
+    /// This may be overridden in the tests via an internal option on `MongoClientOptions`.
     private var minHeartbeatFrequencyMS: Int32 = 500
 
     /// Initializes a new `ConnectionString` with the provided options.
