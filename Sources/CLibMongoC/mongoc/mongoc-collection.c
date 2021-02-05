615680643705834077b27a88a889426776f26450
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2021-02-05 15:12:22 -0500

    Vendor in libmongoc 1.17.4 (#602)

--- Sources/CLibMongoC/mongoc/mongoc-collection.c
+++ Sources/CLibMongoC/mongoc/mongoc-collection.c
@@ -3414,6 +3414,11 @@ retry:
 done:
    mongoc_server_stream_cleanup (server_stream);
    mongoc_server_stream_cleanup (retry_server_stream);
+
+   if (ret && error) {
+      /* if a retry succeeded, clear the initial error */
+      memset (error, 0, sizeof (bson_error_t));
+   }
    mongoc_cmd_parts_cleanup (&parts);
    bson_destroy (&command);
    if (&reply_local == reply_ptr) {
