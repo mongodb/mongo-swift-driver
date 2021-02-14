a7f2ba422440486b2f8474469806f50a4964eed8
Kaitlin Mahar <kaitlinmahar@gmail.com>
2021-02-14 16:05:16 -0500

    Include BSON symbols in driver docs

--- etc/generate-docs.sh
+++ etc/generate-docs.sh
@@ -18,6 +18,12 @@ version=${1}
 # Ensure version is non-empty
 [ ! -z "${version}" ] || { echo "ERROR: Missing version string"; exit 1; }
 
+swift build
+working_dir=${PWD}
+cd .build/checkouts/swift-bson
+sourcekitten doc --spm --module-name SwiftBSON > ${working_dir}/bson-docs.json
+cd $working_dir
+
 jazzy_args=(--clean
             --github-file-prefix https://github.com/mongodb/mongo-swift-driver/tree/v${version} 
             --module-version "${version}")
@@ -25,7 +31,7 @@ jazzy_args=(--clean
 # Generate MongoSwift docs
 sourcekitten doc --spm --module-name MongoSwift > mongoswift-docs.json
 args=("${jazzy_args[@]}"  --output "docs-temp/MongoSwift" --module "MongoSwift" --config ".jazzy.yml" 
-        --sourcekitten-sourcefile mongoswift-docs.json
+        --sourcekitten-sourcefile mongoswift-docs.json,bson-docs.json 
         --root-url "https://mongodb.github.io/mongo-swift-driver/docs/MongoSwift/")
 jazzy "${args[@]}"
 
@@ -37,12 +43,24 @@ python3 etc/filter_sourcekitten_output.py
 sourcekitten doc --spm --module-name MongoSwiftSync > mongoswiftsync-docs.json
 
 args=("${jazzy_args[@]}"  --output "docs-temp/MongoSwiftSync" --module "MongoSwiftSync" --config ".jazzy.yml" 
-        --sourcekitten-sourcefile mongoswift-filtered.json,mongoswiftsync-docs.json
+        --sourcekitten-sourcefile mongoswift-filtered.json,mongoswiftsync-docs.json,bson-docs.json 
         --root-url "https://mongodb.github.io/mongo-swift-driver/docs/MongoSwiftSync/")
 jazzy "${args[@]}"
 
 rm mongoswift-docs.json
 rm mongoswift-filtered.json
 rm mongoswiftsync-docs.json
+rm bson-docs.json
 
 echo '<html><head><meta http-equiv="refresh" content="0; url=MongoSwift/index.html" /></head></html>' > docs-temp/index.html
+
+# we can only pass a single GitHub file prefix above, so we need to correct the BSON file paths throughout the docs.
+
+# obtain BSON version from Package.resolved
+bson_version="$(python3 etc/get_bson_version.py)"
+# since we used the copy of BSON in .build/checkouts, look for all paths in that form throughout HTML files and replace them
+# with the correct path to the BSON repo.
+# note: we have to pass -print0 to `find` and pass -0 to `xargs` because some of the file names have spaces in them, which by
+# default xargs will treat as a delimiter.
+find docs-temp -name "*.html" -print0 | \
+xargs -0 etc/sed.sh -i "s/mongo-swift-driver\/tree\/v${version}\/.build\/checkouts\/swift-bson/swift-bson\/tree\/v${bson_version}/"
