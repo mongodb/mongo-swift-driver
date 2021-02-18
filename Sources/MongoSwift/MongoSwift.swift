5a9ff1c8a276be927aed74f68603c2d2b38c5a15
Kaitlin Mahar <kaitlinmahar@gmail.com>
2021-02-18 18:20:06 -0500

    SWIFT-985 Add client metadata support

--- Sources/MongoSwift/MongoSwift.swift
+++ Sources/MongoSwift/MongoSwift.swift
@@ -3,12 +3,33 @@ import CLibMongoC
 /// MongoSwift only supports MongoDB 3.6+.
 internal let MIN_SUPPORTED_WIRE_VERSION = 6
 
+/// Store optionally provided metadata about a library wrapping the driver.
+private var clientMetadataLibraryName: String? 
+private var clientMetadataLibraryVersion: String?
+
+/// Adds metadata to include the in the handshake performed with the MongoDB server. This is intended for use by
+/// libraries wrapping the driver e.g. MongoDBVapor or an ORM. If used, this method should be called exactly once.
+/// This method will only have an effect if called before any `MongoClient`s are initialized.
+public func addWrappingLibraryMetadata(name: String, version: String) {
+    clientMetadataLibraryName = name
+    clientMetadataLibraryVersion = version
+}
 private final class MongocInitializer {
     internal static let shared = MongocInitializer()
 
     private init() {
         mongoc_init()
-        mongoc_handshake_data_append("MongoSwift", MongoSwiftVersionString, nil)
+        var libraryName = "MongoSwift"
+        if let additionalName = clientMetadataLibraryName {
+            libraryName += " / \(additionalName)"
+        }
+
+        var libraryVersion = MongoSwiftVersionString
+        if let additionalVersion = clientMetadataLibraryVersion {
+            libraryVersion += " / \(additionalVersion)"
+        }
+
+        mongoc_handshake_data_append(libraryName, libraryVersion, nil)
     }
 }
 
--- etc/generated_headers/mongoc-version.h
+++ etc/generated_headers/mongoc-version.h
@@ -31,7 +31,7 @@
  *
  * MONGOC minor version component (e.g. 2 if %MONGOC_VERSION is 1.2.3)
  */
-#define MONGOC_MINOR_VERSION (16)
+#define MONGOC_MINOR_VERSION (17)
 
 
 /**
@@ -39,7 +39,7 @@
  *
  * MONGOC micro version component (e.g. 3 if %MONGOC_VERSION is 1.2.3)
  */
-#define MONGOC_MICRO_VERSION (0)
+#define MONGOC_MICRO_VERSION (4)
 
 
 /**
@@ -47,7 +47,7 @@
  *
  * MONGOC prerelease version component (e.g. pre if %MONGOC_VERSION is 1.2.3-pre)
  */
-#define MONGOC_PRERELEASE_VERSION (pre)
+#define MONGOC_PRERELEASE_VERSION ()
 
 
 /**
@@ -55,7 +55,7 @@
  *
  * MONGOC version.
  */
-#define MONGOC_VERSION (1.16.0-pre)
+#define MONGOC_VERSION (1.17.4)
 
 
 /**
@@ -64,7 +64,7 @@
  * MONGOC version, encoded as a string, useful for printing and
  * concatenation.
  */
-#define MONGOC_VERSION_S "1.16.0-pre"
+#define MONGOC_VERSION_S "1.17.4"
 
 
 /**
