60caa3e74f7ec2bc396bbf428d1d3dfd8e0dc670
Bynn Lee <bynnbynn@gmail.com>
2021-03-22 18:57:56 -0400

    Update Sources/MongoSwift/MongoCollection+FindAndModify.swift
    Co-authored-by: Kaitlin Mahar <kaitlin.mahar@mongodb.com>

--- Sources/MongoSwift/MongoCollection+FindAndModify.swift
+++ Sources/MongoSwift/MongoCollection+FindAndModify.swift
@@ -198,7 +198,7 @@ public struct FindOneAndReplaceOptions: FindAndModifyOptionsConvertible, Decodab
     /// Specifies a collation to use.
     public var collation: BSONDocument?
 
-    /// A document or string that specifies the index to use to support the query. Only supported in server 4.4+
+    /// A document or string that specifies the index to use to support the query. Only supported in server 4.4+.
     public var hint: IndexHint?
 
     /// The maximum amount of time to allow the query to run.
