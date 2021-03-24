c51afbe9c6009b42ee7f966f0bd24cd1951b98f5
Kaitlin Mahar <kaitlinmahar@gmail.com>
2021-03-23 21:53:11 -0400

    don't run tests yet

--- Tests/MongoSwiftSyncTests/VersionedAPITests.swift
+++ Tests/MongoSwiftSyncTests/VersionedAPITests.swift
@@ -4,18 +4,10 @@ import TestsCommon
 
 final class VersionedAPITests: MongoSwiftTestCase {
     func testVersionedAPI() throws {
-        let tests = try retrieveSpecTestFiles(
+        // just test that we can decode the tests for now.
+        _ = try retrieveSpecTestFiles(
             specName: "versioned-api",
             asType: UnifiedTestFile.self
         ).map { $0.1 }
-
-        let runner = try UnifiedTestRunner()
-        let skipTests = [
-            // TODO SWIFT-1099: unskip these once we have vendored in C code that handles this.
-            "CRUD Api Version 1 (strict)": ["estimatedDocumentCount appends declared API version"],
-            "CRUD Api Version 1": ["estimatedDocumentCount appends declared API version on 4.9.0 or greater"]
-        ]
-
-        try runner.runFiles(tests, skipTests: skipTests)
     }
 }
