dfa2ca903ed2aba72482c59b033b84666fab2c67
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2021-04-08 11:51:38 -0400

    Fix index tests to pass on server latest (#628)

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -191,6 +191,7 @@ extension MongoCollection_IndexTests {
     static var allTests = [
         ("testCreateIndexFromModel", testCreateIndexFromModel),
         ("testIndexOptions", testIndexOptions),
+        ("testTextIndex", testTextIndex),
         ("testIndexWithWildCard", testIndexWithWildCard),
         ("testCreateIndexesFromModels", testCreateIndexesFromModels),
         ("testCreateIndexFromKeys", testCreateIndexFromKeys),
--- Tests/MongoSwiftSyncTests/MongoCollection+IndexTests.swift
+++ Tests/MongoSwiftSyncTests/MongoCollection+IndexTests.swift
@@ -77,18 +77,14 @@ final class MongoCollection_IndexTests: MongoSwiftTestCase {
             background: true,
             bits: 32,
             collation: ["locale": "fr"],
-            defaultLanguage: "english",
-            languageOverride: "cat",
             max: 30,
             min: 0,
             name: "testOptions",
             sparse: false,
             sphereIndexVersion: 2,
             storageEngine: ["wiredTiger": ["configString": "access_pattern_hint=random"]],
-            textIndexVersion: 2,
             unique: true,
-            version: 2,
-            weights: ["cat": 0.5, "_id": 0.5]
+            version: 2
         )
 
         // option is no longer supported as of SERVER-47081
@@ -123,6 +119,20 @@ final class MongoCollection_IndexTests: MongoSwiftTestCase {
         var expectedTtlOptions = ttlOptions
         expectedTtlOptions.version = 2
         expect(indexOptions[2]).to(equal(expectedTtlOptions))
+    }
+
+    func testTextIndex() throws {
+        let textIndexOpts = IndexOptions(
+            defaultLanguage: "english",
+            languageOverride: "languageOverrideField",
+            name: "myTextIndex",
+            textIndexVersion: 2,
+            weights: ["cat": 2, "dog": 1]
+        )
+
+        defer { try? self.coll.dropIndex("myTextIndex") }
+        let model = IndexModel(keys: ["cat": "text", "dog": "text"], options: textIndexOpts)
+        expect(try self.coll.createIndex(model)).to(equal("myTextIndex"))
     }
 
     func testIndexWithWildCard() throws {
