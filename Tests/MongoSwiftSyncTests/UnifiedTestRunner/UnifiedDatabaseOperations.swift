94ae6c2174a04bf12ba606acbbbb110b471a8412
Kaitlin Mahar <kaitlinmahar@gmail.com>
2021-07-07 17:18:37 -0400

    add runCommand op to unified runner

--- Tests/MongoSwiftSyncTests/UnifiedTestRunner/UnifiedDatabaseOperations.swift
+++ Tests/MongoSwiftSyncTests/UnifiedTestRunner/UnifiedDatabaseOperations.swift
@@ -1,3 +1,4 @@
+import MongoSwiftSync
 struct UnifiedCreateCollection: UnifiedOperationProtocol {
     /// The collection to create.
     let collection: String
@@ -33,5 +34,28 @@ struct UnifiedDropCollection: UnifiedOperationProtocol {
         let session = try context.entities.resolveSession(id: self.session)
         try db.collection(self.collection).drop(session: session)
         return .none
+    }
+}
+
+struct UnifiedRunCommand: UnifiedOperationProtocol {
+    /// The command to run.
+    let command: BSONDocument
+    
+    /// The name of the command to run. Used for reordering the document to put the command name first.
+    let commandName: String
+
+    static var knownArguments: Set<String> {
+        ["command", "commandName"]
+    }
+
+    func execute(on object: UnifiedOperation.Object, context: Context) throws -> UnifiedOperationResult {
+        let db = try context.entities.getEntity(from: object).asDatabase()
+        var ordered: BSONDocument = [:]
+        ordered[commandName] = self.command[commandName]!
+        for (k, v) in self.command where k != commandName {
+            ordered[k] = v
+        }
+        let result = try db.runCommand(ordered)
+        return .rootDocument(result)
     }
 }
