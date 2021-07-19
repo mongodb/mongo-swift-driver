426f65cb7cbe89c2656b6f77462881b35981c263
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2021-07-19 18:01:49 -0400

    Update Vapor example to depend on MongoDBVapor 1.0.0+, and pin js-bson version (#647)

--- Examples/VaporExample/Package.swift
+++ Examples/VaporExample/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "4.7.0")),
         .package(url: "https://github.com/vapor/leaf", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/mongodb/mongodb-vapor", .exact("1.0.0-beta.0"))
+        .package(url: "https://github.com/mongodb/mongodb-vapor", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
