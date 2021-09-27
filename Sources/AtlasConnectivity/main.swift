6b4dda7c8c3d19d62ebf644e299596f11df0e939
Kaitlin Mahar <kaitlinmahar@gmail.com>
2021-09-27 18:17:09 -0400

    Remove legacy hello workaround, add serverless instance tests

--- Sources/AtlasConnectivity/main.swift
+++ Sources/AtlasConnectivity/main.swift
@@ -1,11 +1,8 @@
 import Foundation
 import MongoSwiftSync
 
-private let configs = ["ATLAS_REPL", "ATLAS_SHRD", "ATLAS_FREE", "ATLAS_TLS11", "ATLAS_TLS12"]
+private let configs = ["ATLAS_REPL", "ATLAS_SHRD", "ATLAS_FREE", "ATLAS_TLS11", "ATLAS_TLS12", "ATLAS_SERVERLESS"]
 private let srvConfigs = configs.map { $0 + "_SRV" }
-/// Currently, almost all of the Atlas test instances are running 3.4 which does not support the new "hello" command.
-private let legacyHello = "ismaster"
-private let supportsHello = ["ATLAS_FREE", "ATLAS_FREE_SRV"]
 
 for config in configs + srvConfigs {
     print("Testing config \(config)... ", terminator: "")
@@ -19,11 +16,7 @@ for config in configs + srvConfigs {
         let client = try MongoClient(uri)
         // run legacy hello command
         let db = client.db("test")
-        if supportsHello.contains(config) {
-            _ = try db.runCommand(["hello": 1])
-        } else {
-            _ = try db.runCommand([legacyHello: 1])
-        }
+        _ = try db.runCommand(["hello": 1])
 
         // findOne
         let coll = db.collection("test")
