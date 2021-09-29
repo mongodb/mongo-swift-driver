459e30d53033d2f58671231a506a5612905de275
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2021-09-29 11:10:09 -0400

    SWIFT-1250 Add Atlas serverless connectivity tests (#677)

--- Sources/AtlasConnectivity/main.swift
+++ Sources/AtlasConnectivity/main.swift
@@ -1,11 +1,8 @@
 import Foundation
 import MongoSwiftSync
 
-private let configs = ["ATLAS_REPL", "ATLAS_SHRD", "ATLAS_FREE", "ATLAS_TLS11", "ATLAS_TLS12"]
+private let configs = ["ATLAS_REPL", "ATLAS_SHRD", "ATLAS_FREE", "ATLAS_TLS11", "ATLAS_TLS12", "ATLAS_SERVERLESS"]
 private let srvConfigs = configs.map { $0 + "_SRV" }
-/// Currently, almost all of the Atlas test instances are running 3.4 which does not support the new "hello" command.
-private let legacyHello = "ismaster"
-private let supportsHello = ["ATLAS_FREE", "ATLAS_FREE_SRV"]
 
 for config in configs + srvConfigs {
     print("Testing config \(config)... ", terminator: "")
@@ -17,13 +14,8 @@ for config in configs + srvConfigs {
 
     do {
         let client = try MongoClient(uri)
-        // run legacy hello command
         let db = client.db("test")
-        if supportsHello.contains(config) {
-            _ = try db.runCommand(["hello": 1])
-        } else {
-            _ = try db.runCommand([legacyHello: 1])
-        }
+        _ = try db.runCommand(["hello": 1])
 
         // findOne
         let coll = db.collection("test")
