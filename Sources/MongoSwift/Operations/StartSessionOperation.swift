29d9e2ae2407591a63259cba18d6605c6d30a100
Kaitlin Mahar <kaitlinmahar@gmail.com>
2021-10-19 22:39:40 -0400

    support snapshot option

--- Sources/MongoSwift/Operations/StartSessionOperation.swift
+++ Sources/MongoSwift/Operations/StartSessionOperation.swift
@@ -3,9 +3,13 @@ import Foundation
 
 /// Options to use when creating a `ClientSession`.
 public struct ClientSessionOptions {
-    /// Whether to enable causal consistency for this session. By default, causal consistency is enabled.
-    ///
-    /// - SeeAlso: https://docs.mongodb.com/manual/core/read-isolation-consistency-recency/
+    /**
+     * Whether to enable causal consistency for this session. By default, if `snapshot` is not set to true, then
+     * causal consistency is enabled. Note that setting this option to true is incompatible with setting `snapshot` to
+     * rue.
+     *
+     * - SeeAlso: https://docs.mongodb.com/manual/core/read-isolation-consistency-recency/
+     */
     public var causalConsistency: Bool?
 
     /// The default `TransactionOptions` to use for transactions started on this session.
@@ -16,10 +20,24 @@ public struct ClientSessionOptions {
     /// applicable (e.g. write concern).
     public var defaultTransactionOptions: TransactionOptions?
 
+    /**
+     * If true, then all reads performed using this session will read from the same snapshot. This option defaults to
+     * false.
+     *
+     * Note that setting this option to true is incompatible with setting `causalConsistency` to true.
+     * - SeeAlso: https://docs.mongodb.com/manual/reference/read-concern-snapshot/
+     */
+    public var snapshot: Bool?
+
     /// Convenience initializer allowing any/all parameters to be omitted.
-    public init(causalConsistency: Bool? = nil, defaultTransactionOptions: TransactionOptions? = nil) {
+    public init(
+        causalConsistency: Bool? = nil,
+        defaultTransactionOptions: TransactionOptions? = nil,
+        snapshot: Bool? = nil
+    ) {
         self.causalConsistency = causalConsistency
         self.defaultTransactionOptions = defaultTransactionOptions
+        self.snapshot = snapshot
     }
 }
 
@@ -39,6 +57,10 @@ private func withSessionOpts<T>(
 
     withMongocTransactionOpts(copying: options?.defaultTransactionOptions) {
         mongoc_session_opts_set_default_transaction_opts(opts, $0)
+    }
+
+    if let snapshot = options?.snapshot {
+        mongoc_session_opts_set_snapshot(opts, snapshot)
     }
 
     return try body(opts)
