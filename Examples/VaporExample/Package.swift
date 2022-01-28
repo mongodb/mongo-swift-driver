efe1cf57f755c83e3cd438e5173e1403a8163853
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2022-01-28 16:38:57 -0500

    SWIFT-1396 Update Vapor example to use async/await (#726)

--- Examples/VaporExample/Package.swift
+++ Examples/VaporExample/Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "VaporExample",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v12)
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "4.7.0")),
+        .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "4.50.0")),
         .package(url: "https://github.com/vapor/leaf", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/mongodb/mongodb-vapor", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/mongodb/mongodb-vapor", .exact("1.1.0-alpha.1"))
     ],
     targets: [
         .target(
@@ -18,9 +18,16 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Leaf", package: "leaf"),
                 .product(name: "MongoDBVapor", package: "mongodb-vapor")
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. For details, see
+                // <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production>.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
-        .target(name: "Run", dependencies: [
+        .executableTarget(name: "Run", dependencies: [
             .target(name: "App"),
             .product(name: "MongoDBVapor", package: "mongodb-vapor")
         ])
--- Examples/VaporExample/Sources/App/routes.swift
+++ Examples/VaporExample/Sources/App/routes.swift
@@ -4,19 +4,17 @@ import Vapor
 /// Adds a collection of web routes to the application.
 func webRoutes(_ app: Application) throws {
     /// Handles a request to load the main index page containing a list of kittens.
-    app.get { req -> EventLoopFuture<View> in
-        req.findKittens().flatMap { kittens in
-            // Return the corresponding Leaf view, providing the list of kittens as context.
-            req.view.render("index.leaf", ["kittens": kittens])
-        }
+    app.get { req async throws -> View in
+        let kittens = try await req.findKittens()
+        // Return the corresponding Leaf view, providing the list of kittens as context.
+        return try await req.view.render("index.leaf", ["kittens": kittens])
     }
 
     /// Handles a request to load a page with info about a particular kitten.
-    app.get("kittens", ":name") { req -> EventLoopFuture<View> in
-        try req.findKitten().flatMap { kitten in
-            // Return the corresponding Leaf view, providing the kitten as context.
-            req.view.render("kitten.leaf", kitten)
-        }
+    app.get("kittens", ":name") { req async throws -> View in
+        let kitten = try await req.findKitten()
+        // Return the corresponding Leaf view, providing the kitten as context.
+        return try await req.view.render("kitten.leaf", kitten)
     }
 }
 
@@ -25,33 +23,33 @@ func restAPIRoutes(_ app: Application) throws {
     let rest = app.grouped("rest")
 
     /// Handles a request to load the list of kittens.
-    rest.get { req -> EventLoopFuture<[Kitten]> in
-        req.findKittens()
+    rest.get { req async throws -> [Kitten] in
+        try await req.findKittens()
     }
 
     /// Handles a request to add a new kitten.
-    rest.post { req -> EventLoopFuture<Response> in
-        try req.addKitten()
+    rest.post { req async throws -> Response in
+        try await req.addKitten()
     }
 
     /// Handles a request to load info about a particular kitten.
-    rest.get("kittens", ":name") { req -> EventLoopFuture<Kitten> in
-        try req.findKitten()
+    rest.get("kittens", ":name") { req async throws -> Kitten in
+        try await req.findKitten()
     }
 
-    rest.delete("kittens", ":name") { req -> EventLoopFuture<Response> in
-        try req.deleteKitten()
+    rest.delete("kittens", ":name") { req async throws -> Response in
+        try await req.deleteKitten()
     }
 
-    rest.patch("kittens", ":name") { req -> EventLoopFuture<Response> in
-        try req.updateKitten()
+    rest.patch("kittens", ":name") { req async throws -> Response in
+        try await req.updateKitten()
     }
 }
 
 extension Request {
-    /// Convenience extension for obtaining a collection which uses the same event loop as a request.
+    /// Convenience extension for obtaining a collection.
     var kittenCollection: MongoCollection<Kitten> {
-        self.mongoDB.client.db("home").collection("kittens", withType: Kitten.self)
+        self.application.mongoDB.client.db("home").collection("kittens", withType: Kitten.self)
     }
 
     /// Constructs a document using the name from this request which can be used a filter for MongoDB
@@ -65,69 +63,73 @@ extension Request {
         return ["name": .string(name)]
     }
 
-    func findKittens() -> EventLoopFuture<[Kitten]> {
-        self.kittenCollection.find().flatMap { cursor in
-            cursor.toArray()
-        }.flatMapErrorThrowing { error in
+    func findKittens() async throws -> [Kitten] {
+        do {
+            return try await self.kittenCollection.find().toArray()
+        } catch {
             throw Abort(.internalServerError, reason: "Failed to load kittens: \(error)")
         }
     }
 
-    func findKitten() throws -> EventLoopFuture<Kitten> {
+    func findKitten() async throws -> Kitten {
         let nameFilter = try self.getNameFilter()
-        return self.kittenCollection.findOne(nameFilter)
-            .unwrap(or: Abort(.notFound, reason: "No kitten with matching name"))
+        guard let kitten = try await self.kittenCollection.findOne(nameFilter) else {
+            throw Abort(.notFound, reason: "No kitten with matching name")
+        }
+        return kitten
     }
 
-    func addKitten() throws -> EventLoopFuture<Response> {
+    func addKitten() async throws -> Response {
         let newKitten = try self.content.decode(Kitten.self)
-        return self.kittenCollection.insertOne(newKitten)
-            .map { _ in Response(status: .created) }
-            .flatMapErrorThrowing { error in
-                // Give a more helpful error message in case of a duplicate key error.
-                if let err = error as? MongoError.WriteError, err.writeFailure?.code == 11000 {
-                    throw Abort(.conflict, reason: "A kitten with the name \(newKitten.name) already exists!")
-                }
-                throw Abort(.internalServerError, reason: "Failed to save new kitten: \(error)")
+        do {
+            try await self.kittenCollection.insertOne(newKitten)
+            return Response(status: .created)
+        } catch {
+            // Give a more helpful error message in case of a duplicate key error.
+            if let err = error as? MongoError.WriteError, err.writeFailure?.code == 11000 {
+                throw Abort(.conflict, reason: "A kitten with the name \(newKitten.name) already exists!")
             }
+            throw Abort(.internalServerError, reason: "Failed to save new kitten: \(error)")
+        }
     }
 
-    func deleteKitten() throws -> EventLoopFuture<Response> {
+    func deleteKitten() async throws -> Response {
         let nameFilter = try self.getNameFilter()
-        return self.kittenCollection.deleteOne(nameFilter)
-            .flatMapErrorThrowing { error in
-                throw Abort(.internalServerError, reason: "Failed to delete kitten: \(error)")
+        do {
+            // since we aren't using an unacknowledged write concern we can expect deleteOne to return a non-nil result.
+            guard let result = try await self.kittenCollection.deleteOne(nameFilter) else {
+                throw Abort(.internalServerError, reason: "Unexpectedly nil response from database")
             }
-            // since we are not using an unacknowledged write concern we can expect deleteOne to return
-            // a non-nil result.
-            .unwrap(or: Abort(.internalServerError, reason: "Unexpectedly nil response from database"))
-            .flatMapThrowing { result in
-                guard result.deletedCount == 1 else {
-                    throw Abort(.notFound, reason: "No kitten with matching name")
-                }
-                return Response(status: .ok)
+            guard result.deletedCount == 1 else {
+                throw Abort(.notFound, reason: "No kitten with matching name")
             }
+            return Response(status: .ok)
+        } catch {
+            throw Abort(.internalServerError, reason: "Failed to delete kitten: \(error)")
+        }
     }
 
-    func updateKitten() throws -> EventLoopFuture<Response> {
+    func updateKitten() async throws -> Response {
         let nameFilter = try self.getNameFilter()
         // Parse the update data from the request.
         let update = try self.content.decode(KittenUpdate.self)
         /// Create a document using MongoDB update syntax that specifies we want to set a field.
         let updateDocument: BSONDocument = ["$set": .document(try BSONEncoder().encode(update))]
 
-        return self.kittenCollection.updateOne(filter: nameFilter, update: updateDocument)
-            .flatMapErrorThrowing { error in
-                throw Abort(.internalServerError, reason: "Failed to update kitten: \(error)")
+        do {
+            // since we aren't using an unacknowledged write concern we can expect updateOne to return a non-nil result.
+            guard let result = try await self.kittenCollection.updateOne(
+                filter: nameFilter,
+                update: updateDocument
+            ) else {
+                throw Abort(.internalServerError, reason: "Unexpectedly nil response from database")
             }
-            // since we are not using an unacknowledged write concern we can expect updateOne to return
-            // a non-nil result.
-            .unwrap(or: Abort(.internalServerError, reason: "Unexpectedly nil response from database"))
-            .flatMapThrowing { result in
-                guard result.matchedCount == 1 else {
-                    throw Abort(.notFound, reason: "No kitten with matching name")
-                }
-                return Response(status: .ok)
+            guard result.matchedCount == 1 else {
+                throw Abort(.notFound, reason: "No kitten with matching name")
             }
+            return Response(status: .ok)
+        } catch {
+            throw Abort(.internalServerError, reason: "Failed to update kitten: \(error)")
+        }
     }
 }
