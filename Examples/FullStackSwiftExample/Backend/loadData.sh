ab3db4acbf37325938aeeb1571b728901217afed
Kaitlin Mahar <kaitlinmahar@gmail.com>
2022-03-09 13:22:41 -0500

    Check if mongosh is installed in loadData.sh

--- Examples/FullStackSwiftExample/Backend/loadData.sh
+++ Examples/FullStackSwiftExample/Backend/loadData.sh
@@ -1,1 +1,2 @@
+command -v mongosh > /dev/null || echo "Failed to locate mongosh; please follow instructions here to install it: https://docs.mongodb.com/mongodb-shell/install"; exit 1
 mongosh $MONGODB_URI --eval "db.getSiblingDB('home').kittens.insertMany([{name:\"Roscoe\",color:\"orange\", favoriteFood: \"salmon\", lastUpdateTime: new Date()},{name:\"Chester\",color:\"tan\", favoriteFood: \"turkey\", lastUpdateTime: new Date()}]);"
