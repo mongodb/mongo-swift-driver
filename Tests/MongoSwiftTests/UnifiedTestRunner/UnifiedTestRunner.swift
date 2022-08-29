2d8e274aa54f023bd634ad9f50413b5b9bce7e03
Patrick Freed <patrick.freed@mongodb.com>
2022-08-29 14:25:02 -0400

    sort imports

--- Tests/MongoSwiftTests/UnifiedTestRunner/UnifiedTestRunner.swift
+++ Tests/MongoSwiftTests/UnifiedTestRunner/UnifiedTestRunner.swift
@@ -1,8 +1,8 @@
 #if compiler(>=5.5.2) && canImport(_Concurrency)
 import MongoSwift
 import Nimble
-import TestsCommon
 import NIOPosix
+import TestsCommon
 
 @available(macOS 10.15, *)
 class UnifiedTestRunner {
